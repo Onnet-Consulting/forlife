4384fdbdfc720d3f9dbd9d179fa690a2de8e1115
Daihv <daihvls96@gmail.com>
2023-01-05 08:51:15 +0700

    [FIX] forlife_point_of_sale - fix brand name

--- addons/custom/forlife_point_of_sale/models/store.py
+++ addons/custom/forlife_point_of_sale/models/store.py
@@ -11,7 +11,7 @@ class Store(models.Model):
     contact_id = fields.Many2one('res.partner', string='Contact', required=True)
     cashier_ids = fields.Many2many('res.users', string='Cashiers', required=True)
     employee_ids = fields.Many2many('hr.employee', string='Employees')
-    branch_id = fields.Char(string='Branch', required=True) #fixme Many2one đến model branch
+    brand_id = fields.Char(string='Brand', required=True) #fixme Many2one đến model brand
     stock_location_id = fields.Many2one('stock.location', string='Location', required=True)
     pos_config_ids = fields.One2many('pos.config', 'store_id', string='POS Config', readonly=True)
     payment_method_ids = fields.Many2many('pos.payment.method', string='POS Payment Method', required=True)
