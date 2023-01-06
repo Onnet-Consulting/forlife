f8dc997f934859f4ab3003c0e862cc4567e9c6f1
Daihv <daihvls96@gmail.com>
2023-01-06 19:16:29 +0700

    [FIX] forlife_promotion - fix view

--- addons/custom/forlife_promotion/models/points_product_line.py
+++ addons/custom/forlife_promotion/models/points_product_line.py
@@ -7,7 +7,7 @@ class PointsProductLine(models.Model):
     _name = 'points.product.line'
     _description = 'Points Product Line'
 
-    points_product_id = fields.Many2one('points.product', string='Points Product', required=True)
+    points_product_id = fields.Many2one('points.product', string='Products', required=True)
     event_id = fields.Many2one('event', string='Event', ondelete='cascade')
     point_addition = fields.Integer('Point Additions', required=True)
 
