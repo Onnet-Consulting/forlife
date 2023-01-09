1eb0c3027a39567138bbd90954bc375bf111dafb
Daihv <daihvls96@gmail.com>
2023-01-09 08:52:53 +0700

    [REM] forlife_point_of_sale - remove notifications in pos.config kanban view

--- addons/custom/forlife_point_of_sale/models/store.py
+++ addons/custom/forlife_point_of_sale/models/store.py
@@ -17,31 +17,6 @@ class Store(models.Model):
     payment_method_ids = fields.Many2many('pos.payment.method', string='POS Payment Method', required=True)
 
     @api.model
-    def retrieve_dashboard(self):
-        self.check_access_rights('read')
-
-        query = '''with tb as (
-                        select ps.config_id, rp.name from pos_session ps
-                         join res_users ru on ru.id = ps.user_id
-                         join res_partner rp on rp.id = ru.partner_id
-                         where state = 'opened'
-                    )
-                    select pos.name, store.name, tb.name from pos_config pos
-                    join store on pos.store_id = store.id
-                    join tb on tb.config_id = pos.id
-                    where pos.store_id in 
-                        (select store_id from res_users_store_rel where res_users_id = %s)'''
-        self.env.cr.execute(query, (self._uid,))
-        data = self.env.cr.fetchall()
-        group = {}
-        res = []
-        for i in data:
-            group.update({i[1]: group.get(i[1], []) + [i[0] + _(' opened by ') + i[2]]})
-        for k, v in group.items():
-            res.append(_('Store ') + k + ': ' + ', '.join(v))
-        return dict(data='; '.join(res))
-
-    @api.model
     def get_pos_opened(self):
         query = '''SELECT (SELECT name FROM pos_config WHERE id = ps.config_id) FROM pos_session ps 
             WHERE ps.state = 'opened' 
