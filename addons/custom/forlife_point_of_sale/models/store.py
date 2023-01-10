44398b93153ff9d3238c2471c0fd60432427c0b6
Daihv <daihvls96@gmail.com>
2023-01-10 14:41:56 +0700

    [FIX] forlife_point_of_sale - remove _sql_constraints name

--- addons/custom/forlife_point_of_sale/models/store.py
+++ addons/custom/forlife_point_of_sale/models/store.py
@@ -6,7 +6,7 @@ class Store(models.Model):
     _name = 'store'
     _description = 'Store'
 
-    name = fields.Char('Store Name', required=True, copy=False, default=_('New'))
+    name = fields.Char('Store Name', required=True)
     code = fields.Char('Store Code', required=True)  # fixme related trường mã kho trong location
     contact_id = fields.Many2one('res.partner', string='Contact', required=True)
     cashier_ids = fields.Many2many('res.users', string='Cashiers', required=True)
@@ -15,10 +15,6 @@ class Store(models.Model):
     stock_location_id = fields.Many2one('stock.location', string='Location', required=True)
     pos_config_ids = fields.One2many('pos.config', 'store_id', string='POS Config', readonly=True)
     payment_method_ids = fields.Many2many('pos.payment.method', string='POS Payment Method', required=True)
-
-    _sql_constraints = [
-        ('name_uniq', 'unique (name)', 'Store name must be unique !'),
-    ]
 
     @api.model
     def get_pos_opened(self):
