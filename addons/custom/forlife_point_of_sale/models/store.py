eb6f9ed2eae3613d773c555f7ea482ef21b60eda
Daihv <daihvls96@gmail.com>
2023-01-10 09:28:46 +0700

    [ADD] forlife_custom_module - define Brand model

--- addons/custom/forlife_point_of_sale/models/store.py
+++ addons/custom/forlife_point_of_sale/models/store.py
@@ -6,15 +6,19 @@ class Store(models.Model):
     _name = 'store'
     _description = 'Store'
 
-    name = fields.Char('Store Name', required=True)
+    name = fields.Char('Store Name', required=True, copy=False, default=_('New'))
     code = fields.Char('Store Code', required=True)  # fixme related trường mã kho trong location
     contact_id = fields.Many2one('res.partner', string='Contact', required=True)
     cashier_ids = fields.Many2many('res.users', string='Cashiers', required=True)
     employee_ids = fields.Many2many('hr.employee', string='Employees')
-    brand_id = fields.Char(string='Brand', required=True) #fixme Many2one đến model brand
+    x_brand_id = fields.Many2one('brand', string='Brand', required=True) # fixme: để tên x_brand_id tránh gây ra lỗi kiểu dữ liệu khi upgrade module, ban đầu khai báo kiểu Char, sẽ điều chỉnh lại thành brand_id vào lần commit code sau
     stock_location_id = fields.Many2one('stock.location', string='Location', required=True)
     pos_config_ids = fields.One2many('pos.config', 'store_id', string='POS Config', readonly=True)
     payment_method_ids = fields.Many2many('pos.payment.method', string='POS Payment Method', required=True)
+
+    _sql_constraints = [
+        ('name_uniq', 'unique (name)', 'Store name must be unique !'),
+    ]
 
     @api.model
     def get_pos_opened(self):
@@ -26,4 +30,3 @@ class Store(models.Model):
         data = self.env.cr.fetchall()
 
         return data
-
