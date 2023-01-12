bc0499dcbc81bf2f7853ab1805c6550c03efd548
Daihv <daihvls96@gmail.com>
2023-01-12 16:30:20 +0700

    [ADD] forlife_point_of_sale: add company, warehouse for Store model

--- addons/custom/forlife_point_of_sale/models/store.py
+++ addons/custom/forlife_point_of_sale/models/store.py
@@ -7,12 +7,14 @@ class Store(models.Model):
     _description = 'Store'
 
     name = fields.Char('Store Name', required=True)
-    code = fields.Char('Store Code', required=True)  # fixme related trường mã kho trong location
+    company_id = fields.Many2one('res.company', string='Company', required=True, readonly=True, default=lambda self: self.env.company)
+    warehouse_id = fields.Many2one('stock.warehouse', string='Warehouse')
+    code = fields.Char('Store Code', related='warehouse_id.code', store=True)
     contact_id = fields.Many2one('res.partner', string='Contact', required=True)
     cashier_ids = fields.Many2many('res.users', string='Cashiers', required=True)
     employee_ids = fields.Many2many('hr.employee', string='Employees')
     brand_id = fields.Many2one('res.brand', string='Brand', required=True)
-    stock_location_id = fields.Many2one('stock.location', string='Location', required=True, domain=[('usage', '=', 'internal')])
+    stock_location_id = fields.Many2one('stock.location', string='Location', required=True)
     pos_config_ids = fields.One2many('pos.config', 'store_id', string='POS Config', readonly=True)
     payment_method_ids = fields.Many2many('pos.payment.method', string='POS Payment Method', required=True)
 
