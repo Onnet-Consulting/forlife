35b2521edc225bc962279f766ac5ef2ea8635874
ducminh <ducminh>
2023-01-17 15:12:10 +0700

    MFL2201ER-277-FIX:check-group-member

--- addons/custom/forlife_pos_point_order/models/partner_history_point.py
+++ addons/custom/forlife_pos_point_order/models/partner_history_point.py
@@ -6,13 +6,13 @@ class PartnerHistoryPointForLife(models.Model):
 
     _description = 'History of Customer purchase POS'
 
-    point_order_type = fields.Selection([('new', 'Order New'), ('back_order', 'Back Order'),('reset_order', 'Reset Point'),('point compensate', 'Point Compensate')], string='Type')
+    point_order_type = fields.Selection([('new', 'Order New'), ('back_order', 'Back Order'),('reset_order', 'Reset Point'),('point compensate', 'Point Compensate')], string='Type', readonly=True)
 
     partner_id = fields.Many2one('res.partner')
-    date_order = fields.Datetime('Date Order')
-    points_fl_order = fields.Integer('Points Order')
-    points_used = fields.Integer('Points Used')
-    points_back = fields.Integer('Points Back')
-    points_store = fields.Integer('Points Store')
-    reason = fields.Text('Reason')
-    store = fields.Selection([('forlife', 'For Life'), ('format', 'Format')])
+    date_order = fields.Datetime('Date Order', readonly=True)
+    points_fl_order = fields.Integer('Points Order',readonly=True)
+    points_used = fields.Integer('Points Used',readonly=True)
+    points_back = fields.Integer('Points Back',readonly=True)
+    points_store = fields.Integer('Points Store',readonly=True)
+    reason = fields.Text('Reason',readonly=True)
+    store = fields.Selection([('forlife', 'For Life'), ('format', 'Format')], readonly=True)
