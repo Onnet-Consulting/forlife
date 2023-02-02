3932db07b3240d756967a59583fbe96ada3c377f
xmars <xtruongptit@gmail.com>
2023-02-02 16:34:31 +0700

    [FIX] forlife_pos_assign_employee - show employee on refund order line

--- addons/custom/forlife_pos_assign_employee/models/pos_order_line.py
+++ addons/custom/forlife_pos_assign_employee/models/pos_order_line.py
@@ -7,3 +7,8 @@ class PosOrderLine(models.Model):
     _inherit = 'pos.order.line'
 
     employee_id = fields.Many2one('hr.employee', string='Employee', required=True)
+
+    def _export_for_ui(self, orderline):
+        result = super()._export_for_ui(orderline)
+        result['assigned_employee'] = orderline.employee_id.name
+        return result
