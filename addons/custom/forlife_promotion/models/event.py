5798d2566c97a13e7bac0e41e890a875da197b13
ducminh <ducminh>
2023-02-08 10:29:45 +0700

    check-domain-iis-False

--- addons/custom/forlife_promotion/models/event.py
+++ addons/custom/forlife_promotion/models/event.py
@@ -107,3 +107,7 @@ class Event(models.Model):
             })
         if points_products:
             self.write({'points_product_existed': json.dumps(points_products.ids + points_product_existed)})
+
+    def write(self, vals):
+        print(self.customer_conditions)
+        return super(Event, self).write(vals)
