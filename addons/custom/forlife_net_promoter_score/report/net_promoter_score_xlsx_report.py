7facc1860701973c2da871f51ec9c9628d7e63f0
Daihv <daihvls96@gmail.com>
2023-02-09 23:18:50 +0700

    [IMP] forlife_net_promoter_score - add excel report NSP

--- addons/custom/forlife_net_promoter_score/report/net_promoter_score_xlsx_report.py
+++ addons/custom/forlife_net_promoter_score/report/net_promoter_score_xlsx_report.py
@@ -9,179 +9,32 @@ class NPSReportXlsx(models.AbstractModel):
     _inherit = "report.report_xlsx.abstract"
     _description = "NPS Report"
 
-    def generate_xlsx_report(self, workbook, data, comment):
-        if not comment:
-            raise ValidationError(_('Comment not found.'))
-        return True
-        # salary_record = comment[0] if comment else False
-        # sh_header = workbook.add_worksheet("Header")
-        # sh_luong = workbook.add_worksheet("Lương")
-        # sh_ttn = workbook.add_worksheet("Tổng thu nhập")
-        # sh_bh = workbook.add_worksheet("BH+phí CĐ+thuế TNCN cần chi trả")
-        # sh_tt = workbook.add_worksheet("Truy thu NLĐ")
-        # sh_ltd = workbook.add_worksheet("Lương tồn đọng")
-        # _format = workbook.add_format({
-        #     'bold': True,
-        #     'bg_color': '#a8cde3',
-        #     'text_wrap': True,
-        #     'valign': 'vcenter',
-        # })
-        # self.write_sheet_header(sh_header, salary_record, _format)
-        # self.write_sheet_luong(sh_luong, salary_record.salary_record_main_ids, _format)
-        # self.write_sheet_ttn(sh_ttn, salary_record.salary_total_income_ids, _format)
-        # self.write_sheet_bh(sh_bh, salary_record.salary_supplementary_ids, _format)
-        # self.write_sheet_tt(sh_tt, salary_record.salary_arrears_ids, _format)
-        # self.write_sheet_ltd(sh_ltd, salary_record.salary_backlog_ids, _format)
-
-    def write_sheet_header(self, sh_header, salary_record, _format):
-        header = ['Công ty', 'Loại', 'Năm', 'Tháng', 'Mô tả']
-        for i, name in enumerate(header):
-            sh_header.write(0, i, name, _format)
-        sh_header.set_row(0, 25)
-        sh_header.set_column(0, len(header) - 1, 20)
-        sh_header.write(1, 0, salary_record.company_id.name)
-        sh_header.write(1, 1, salary_record.type_id.name)
-        sh_header.write(1, 2, salary_record.year)
-        sh_header.write(1, 3, salary_record.month)
-        sh_header.write(1, 4, salary_record.note or '')
-
-    def write_sheet_luong(self, sh_luong, salary_record_main, _format):
-        header = ['Mục đích tính lương', 'Mã phòng/bp', 'Tên phòng/bộ phận', 'Mã cost center', 'Mã Dự án', 'Tổng thu nhập', 'Ký quỹ', 'TKDP', 'Phạt vi phạm', 'Trừ TTHH', 'Trừ hàng lỗi',
-                  'Trừ chi phí đồng phục FM', 'Phạt doanh số', 'Truy thu lương', 'Truy thu phụ cấp', 'Tạm ứng', 'Truy thu BH vào lương', 'Trừ khác', 'Mức BHXH', 'BHXH+BHBNN-TNLĐ Công ty chi trả',
-                  'BHYT Công ty chi trả', 'BHTN Công ty chi trả', 'TỔNG CHI PHÍ BH Công ty chi trả', 'BHXH NLĐ chi trả', 'BHYT NLĐ chi trả', 'BHTN NLĐ chi trả',
-                  'TỔNG CHI PHÍ BH NLĐ chi trả', 'Công đoàn phí công ty nộp', 'Công đoàn phí NLĐ nộp', 'Thuế TNCN NLĐ nộp', 'Tổng trừ', 'Thực lĩnh', 'Số lượng nhân sự']
-        for i, name in enumerate(header):
-            sh_luong.write(0, i, name, _format)
-        sh_luong.set_row(0, 40)
-        sh_luong.set_column(0, len(header) - 1, 20)
-        for row, value in enumerate(salary_record_main, start=1):
-            sh_luong.write(row, 0, value.purpose_id.name)
-            sh_luong.write(row, 1, value.department_id.code)
-            sh_luong.write(row, 2, value.department_id.name)
-            sh_luong.write(row, 3, value.analytic_account_id.code)
-            sh_luong.write(row, 4, value.project_code or '')
-            sh_luong.write(row, 5, value.x_ttn or 0)
-            sh_luong.write(row, 6, value.x_kq or 0)
-            sh_luong.write(row, 7, value.x_tkdp or 0)
-            sh_luong.write(row, 8, value.x_pvp or 0)
-            sh_luong.write(row, 9, value.x_tthh or 0)
-            sh_luong.write(row, 10, value.x_thl or 0)
-            sh_luong.write(row, 11, value.x_dpfm or 0)
-            sh_luong.write(row, 12, value.x_pds or 0)
-            sh_luong.write(row, 13, value.x_ttl or 0)
-            sh_luong.write(row, 14, value.x_ttpc or 0)
-            sh_luong.write(row, 15, value.x_tu or 0)
-            sh_luong.write(row, 16, value.x_ttbh or 0)
-            sh_luong.write(row, 17, value.x_tk or 0)
-            sh_luong.write(row, 18, value.x_bhxh_level or 0)
-            sh_luong.write(row, 19, value.x_bhxh_bhbnn_tnld_ct or 0)
-            sh_luong.write(row, 20, value.x_bhyt_ct or 0)
-            sh_luong.write(row, 21, value.x_bhtn_ct or 0)
-            sh_luong.write(row, 22, value.x_tbh_ct or 0)
-            sh_luong.write(row, 23, value.x_bhxh_nld or 0)
-            sh_luong.write(row, 24, value.x_bhyt_nld or 0)
-            sh_luong.write(row, 25, value.x_bhtn_nld or 0)
-            sh_luong.write(row, 26, value.x_tbh_nld or 0)
-            sh_luong.write(row, 27, value.x_cdp_ct or 0)
-            sh_luong.write(row, 28, value.x_cdp_nld or 0)
-            sh_luong.write(row, 29, value.x_tncn or 0)
-            sh_luong.write(row, 30, value.x_tt or 0)
-            sh_luong.write(row, 31, value.x_tl or 0)
-            sh_luong.write(row, 32, value.x_slns or 0)
-
-    def write_sheet_ttn(self, sh_ttn, salary_total_income, _format):
-        header = [' Mục đích tính lương', 'Mã phòng/bp', 'Tên phòng/bộ phận', 'Mã cost center', 'Mã Dự án', 'Mã lệnh sản xuất', 'Mã chương trình SK', 'Tổng thu nhập', 'Ghi chú']
-        for i, name in enumerate(header):
-            sh_ttn.write(0, i, name, _format)
-        sh_ttn.set_row(0, 40)
-        sh_ttn.set_column(0, len(header) - 1, 20)
-        for row, value in enumerate(salary_total_income, start=1):
-            sh_ttn.write(row, 0, value.purpose_id.name)
-            sh_ttn.write(row, 1, value.department_id.code)
-            sh_ttn.write(row, 2, value.department_id.name)
-            sh_ttn.write(row, 3, value.analytic_account_id.code)
-            sh_ttn.write(row, 4, value.project_code or '')
-            sh_ttn.write(row, 5, value.manufacture_order_code or '')
-            sh_ttn.write(row, 6, value.internal_order_code or '')
-            sh_ttn.write(row, 7, value.x_ttn or 0)
-            sh_ttn.write(row, 8, value.note or '')
-
-    def write_sheet_bh(self, sh_bh, salary_supplementary, _format):
-        header = ['Mục đích tính lương', 'Mã phòng/bp', 'Tên phòng/bộ phận', 'Mã cost center', 'Mã dự án', 'Mã lệnh sản xuất', 'Mã chương trình SK', 'Mức BHXH',
-                  'BHXH NLĐ chi trả', 'BHYT NLĐ chi trả', 'BHTN NLĐ chi trả', 'TỔNG CHI PHÍ BH NLĐ chi trả', 'BHXH+BHBNN-TNLĐ Công ty chi trả', 'BHYT Công ty chi trả',
-                  'BHTN Công ty chi trả', 'TỔNG CHI PHÍ BH Công ty chi trả', 'Công đoàn phí công ty nộp', 'Công đoàn phí NLĐ nộp', 'Thuế TNCN NLĐ nộp', 'Ghi chú']
-        for i, name in enumerate(header):
-            sh_bh.write(0, i, name, _format)
-        sh_bh.set_row(0, 40)
-        sh_bh.set_column(0, len(header) - 1, 20)
-        for row, value in enumerate(salary_supplementary, start=1):
-            sh_bh.write(row, 0, value.purpose_id.name)
-            sh_bh.write(row, 1, value.department_id.code)
-            sh_bh.write(row, 2, value.department_id.name)
-            sh_bh.write(row, 3, value.analytic_account_id.code)
-            sh_bh.write(row, 4, value.project_code or '')
-            sh_bh.write(row, 5, value.manufacture_order_code or '')
-            sh_bh.write(row, 6, value.internal_order_code or '')
-            sh_bh.write(row, 7, value.x_bhxh_level or 0)
-            sh_bh.write(row, 8, value.x_bhxh_nld or 0)
-            sh_bh.write(row, 9, value.x_bhyt_nld or 0)
-            sh_bh.write(row, 10, value.x_bhtn_nld or 0)
-            sh_bh.write(row, 11, value.x_tbh_nld or 0)
-            sh_bh.write(row, 12, value.x_bhxh_bhbnn_tnld_ct or 0)
-            sh_bh.write(row, 13, value.x_bhyt_ct or 0)
-            sh_bh.write(row, 14, value.x_bhtn_ct or 0)
-            sh_bh.write(row, 15, value.x_tbh_ct or 0)
-            sh_bh.write(row, 16, value.x_cdp_ct or 0)
-            sh_bh.write(row, 17, value.x_cdp_nld or 0)
-            sh_bh.write(row, 18, value.x_tncn or 0)
-            sh_bh.write(row, 19, value.note or '')
-
-    def write_sheet_tt(self, sh_tt, salary_arrears, _format):
-        header = ['Mục đích tính lương', 'Mã NV', 'Họ tên', 'Mã phòng/bp', 'Tên phòng/bộ phận', 'Mã cost center', 'Mã Dự án', 'Mã lệnh sản xuất', 'Mã chương trình SK',
-                  'Ký quỹ', 'TKDP', 'Phạt vi phạm', 'Trừ TTHH', 'Trừ hàng lỗi', 'Trừ đồng phục', 'Phạt doanh số', 'Truy thu lương', 'Truy thu phụ cấp',
-                  'Tạm ứng', 'Trừ khác', 'Công nợ BHXH NLĐ chi trả', 'Công nợ BHYT NLĐ chi trả', 'Công nợ BHTN NLĐ chi trả', 'Truy thu BH vào lương', 'Ghi chú ']
-        for i, name in enumerate(header):
-            sh_tt.write(0, i, name, _format)
-        sh_tt.set_row(0, 40)
-        sh_tt.set_column(0, len(header) - 1, 20)
-        for row, value in enumerate(salary_arrears, start=1):
-            sh_tt.write(row, 0, value.purpose_id.name)
-            sh_tt.write(row, 1, value.employee_id.barcode)
-            sh_tt.write(row, 2, value.employee_id.name)
-            sh_tt.write(row, 3, value.department_id.code)
-            sh_tt.write(row, 4, value.department_id.name)
-            sh_tt.write(row, 5, value.analytic_account_id.code)
-            sh_tt.write(row, 6, value.project_code or '')
-            sh_tt.write(row, 7, value.manufacture_order_code or '')
-            sh_tt.write(row, 8, value.internal_order_code or '')
-            sh_tt.write(row, 9, value.x_kq or 0)
-            sh_tt.write(row, 10, value.x_tkdp or 0)
-            sh_tt.write(row, 11, value.x_pvp or 0)
-            sh_tt.write(row, 12, value.x_tthh or 0)
-            sh_tt.write(row, 13, value.x_thl or 0)
-            sh_tt.write(row, 14, value.x_dpfm or 0)
-            sh_tt.write(row, 15, value.x_pds or 0)
-            sh_tt.write(row, 16, value.x_ttl or 0)
-            sh_tt.write(row, 17, value.x_ttpc or 0)
-            sh_tt.write(row, 18, value.x_tu or 0)
-            sh_tt.write(row, 19, value.x_tk or 0)
-            sh_tt.write(row, 20, value.x_bhxh_cn or 0)
-            sh_tt.write(row, 21, value.x_bhyt_cn or 0)
-            sh_tt.write(row, 22, value.x_bhxh_bhbnn_tnld_cn or 0)
-            sh_tt.write(row, 23, value.x_ttbh or 0)
-            sh_tt.write(row, 24, value.note or '')
-
-    def write_sheet_ltd(self, sh_ltd, salary_backlog, _format):
-        header = ['STT', 'Mã NV', 'Họ tên', 'Mã phòng/bộ phận', 'Tên phòng/bộ phận', 'Số tiền', 'Tháng']
-        for i, name in enumerate(header):
-            sh_ltd.write(0, i, name, _format)
-        sh_ltd.set_row(0, 25)
-        sh_ltd.set_column(0, len(header) - 1, 20)
-        for row, value in enumerate(salary_backlog, start=1):
-            sh_ltd.write(row, 0, row)
-            sh_ltd.write(row, 1, value.employee_id.barcode)
-            sh_ltd.write(row, 2, value.employee_id.name)
-            sh_ltd.write(row, 3, value.department_id.code)
-            sh_ltd.write(row, 4, value.department_id.name)
-            sh_ltd.write(row, 5, value.amount or 0)
-            sh_ltd.write(row, 6, value.period or '')
+    def generate_xlsx_report(self, workbook, data, record):
+        if not record:
+            raise ValidationError(_('NPS Report values not found.'))
+        result = record[0].filter_data()
+        sheet = workbook.add_worksheet("NPS Report")
+        _format_header = workbook.add_format({
+            'bold': True,
+            'bg_color': '#a8cde3',
+            'text_wrap': True,
+            'valign': 'vcenter',
+            'align': 'center',
+        })
+        _format_center = workbook.add_format({
+            'align': 'center',
+        })
+        sheet.set_row(0, 25)
+        sheet.set_column(1, len(result[0]) - 1, 20)
+        row = 0
+        for value in result:
+            if row == 0:
+                for i, val in enumerate(value):
+                    sheet.write(row, i, val, _format_header)
+            else:
+                for i, val in enumerate(value):
+                    if i in (0, 5, 6, 7, 9):
+                        sheet.write(row, i, val, _format_center)
+                    else:
+                        sheet.write(row, i, val)
+            row += 1
--- addons/custom/forlife_net_promoter_score/wizard/net_promoter_score_report.py
+++ addons/custom/forlife_net_promoter_score/wizard/net_promoter_score_report.py
@@ -1,25 +1,42 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models, _
-from odoo.exceptions import ValidationError
 import pytz
+
+REPORT_HEADER = ['STT', 'Chi nhánh', 'Mã KH', 'Tên KH', 'Mã HĐ', 'Ngày mua hàng', 'Ngày đánh giá', 'Đánh giá', 'Bình luận', 'Trạng thái']
 
 
 class NetPromoterScoreReport(models.TransientModel):
     _name = 'net.promoter.score.report'
     _description = 'Net Promoter Score Report'
 
+    def _default_header(self):
+        result = self.get_view_header()
+        return result[0] + result[1]
+
     name = fields.Char('Name', default='.')
-    from_date = fields.Datetime('From Date')
+    from_date = fields.Datetime('Date')
     to_date = fields.Datetime('To Date')
     customer_code = fields.Text('Customer Code')
     invoice_number = fields.Text('Invoice Number')
-    min_point = fields.Integer('Min Point', default=0)
+    min_point = fields.Integer('Level', default=0)
     max_point = fields.Integer('Max Point', default=100)
-    view_report = fields.Html('View Report')
+    view_report = fields.Html('View Report', default=_default_header)
+
+    def get_view_header(self):
+        return [f'<table class="table table-bordered"><tr style="text-align: center; background: #031d74c7; color: #ffffff;"><th>{"</th><th>".join(REPORT_HEADER)}</th></tr>', '</table>']
 
     def btn_search(self):
-        domain = [('status', '=', '1')]
+        result = self.filter_data()
+        data = ''
+        for line in result[1:]:
+            style = ' style="background: #0a0a0a3d;"' if int(line[0]) % 2 == 0 else ''
+            data += f'<tr{style}><td style="text-align: center;">{"</td><td>".join(line)}</td></tr>'
+        header = self.get_view_header()
+        self.view_report = header[0] + data + header[1]
+
+    def filter_data(self):
+        domain = [('status', '=', '1'), ('point', '>=', self.min_point), ('point', '<=', self.max_point)]
         if self.from_date:
             domain += [('invoice_date', '>=', self.from_date)]
         if self.to_date:
@@ -29,22 +46,12 @@ class NetPromoterScoreReport(models.TransientModel):
         if self.invoice_number:
             domain += [('invoice_number', 'in', [i.strip() for i in self.invoice_number.split(',')])]
         result = self.env['forlife.comment'].search(domain)
-        data = ''
-        stt = 1
+        data = [REPORT_HEADER]
+        row = 1
         for line in result:
-            style = ' style="background: #0a0a0a3d;"' if stt % 2 == 0 else ''
-            val = [line.store_name, line.customer_code, line.customer_name, line.invoice_number,
-                   line.invoice_date.astimezone(pytz.timezone(self.env.user.tz)).strftime('%d-%m-%Y'),
-                   line.comment_date.astimezone(pytz.timezone(self.env.user.tz)).strftime('%d-%m-%Y') if line.comment_date else '',
-                   str(line.point), line.comment or '', str(line.status)]
-            data += f'<tr{style}><td style="text-align: center;">{str(stt)}</td><td>{"</td><td>".join(val)}</td></tr>'
-            stt += 1
-        header = ['STT', 'Chi nhánh', 'Mã KH', 'Tên KH', 'Mã HĐ', 'Ngày mua hàng', 'Ngày đánh giá', 'Đánh giá', 'Bình luận', 'Trạng thái']
-        self.view_report = f'<table class="table table-bordered"><tr style="text-align: center; background: #031d74c7; color: #ffffff;">' \
-                           f'<th>{"</th><th>".join(header)}</th></tr>{data}</table>'
-
-    @api.constrains("start_date", "finish_date")
-    def validate_point(self):
-        for record in self:
-            if record.min_point < 0 or record.max_point > 100 or record.min_point > record.max_point:
-                raise ValidationError(_('Invalid Level.'))
+            data.append([str(row), line.store_name, line.customer_code, line.customer_name, line.invoice_number,
+                         line.invoice_date.astimezone(pytz.timezone(self.env.user.tz)).strftime('%d-%m-%Y'),
+                         line.comment_date.astimezone(pytz.timezone(self.env.user.tz)).strftime('%d-%m-%Y') if line.comment_date else '',
+                         str(line.point), line.comment or '', str(line.status)])
+            row += 1
+        return data
