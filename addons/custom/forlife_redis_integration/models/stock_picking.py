99bf7191fe0ed50e4d0a93bc09013193ce35df20
xmars <xtruongptit@gmail.com>
2023-02-10 13:48:00 +0700

    [REM] forlife_redis_integration - send internal transfer to Redis

--- addons/custom/forlife_redis_integration/models/stock_picking.py
+++ addons/custom/forlife_redis_integration/models/stock_picking.py
@@ -1,15 +1,16 @@
 # -*- coding:utf-8 -*-
 
 from odoo import api, fields, models, _
-
+import json
 
 class StockPicking(models.Model):
     _name = 'stock.picking'
     _inherit = ['stock.picking', 'redis.action']
 
+    @api.model_create_multi
     def create(self, vals_list):
         res = super(StockPicking, self).create(vals_list)
         for picking in res.filtered(lambda x: x.picking_type_code == 'internal'):
             # FIXME: send data with correct key (TKL or FM)
-            self.hset('internal_transfer', 'Invoice', picking.name, {"InvoiceNo": picking.id})
+            self.hset('internal_transfer', 'Invoice', picking.name, json.dumps({"InvoiceNo": picking.id}))
         return res
