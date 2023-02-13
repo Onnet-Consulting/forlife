91a4d83d077405d28eee54f45bd336649f23dbdc
xmars <xtruongptit@gmail.com>
2023-02-13 16:13:34 +0700

    [IMP] forlife_redis_integration - translate response from Redis

--- addons/custom/forlife_redis_integration/models/redis_action.py
+++ addons/custom/forlife_redis_integration/models/redis_action.py
@@ -19,7 +19,8 @@ class RedisAction(models.AbstractModel):
                  there are no Redis host attached to that key") % action_key)
 
         return redis.Redis(host=rds_host.host, port=rds_host.port,
-                           db=rds_host.db, password=rds_host.password, username=rds_host.username)
+                           db=rds_host.db, password=rds_host.password, username=rds_host.username,
+                           decode_responses=True)
 
     @api.model
     def hset(self, action_key, hash_name, key, value):
