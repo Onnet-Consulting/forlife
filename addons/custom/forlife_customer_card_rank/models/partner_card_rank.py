048743576d979993831f170ac376c014b5091376
Daihv <daihvls96@gmail.com>
2023-02-14 23:36:58 +0700

    [IMP] forlife_customer_card_rank - update code

--- addons/custom/forlife_customer_card_rank/models/partner_card_rank.py
+++ addons/custom/forlife_customer_card_rank/models/partner_card_rank.py
@@ -28,6 +28,32 @@ class PartnerCardRank(models.Model):
         for line in self:
             line.card_rank_id = line.line_ids and line.line_ids[0].new_card_rank_id.id or self.env.ref('forlife_customer_card_rank.card_rank_member').id
 
+    def generate_card_rank_data(self):
+        res = {}
+        for line in self:
+            _detail = ''
+            for detail in line.line_ids:
+                _detail += f"<tr style=\"text-align: center;\"><td>{detail.order_id and detail.order_id.name or ''}</td>" \
+                         f"<td>{detail.order_date and detail.order_date.astimezone(pytz.timezone(self.env.user.tz)).strftime('%d/%m/%Y') or ''}</td>" \
+                         f"<td style=\"text-align: right !important;\">{'{:20,.0f}'.format(detail.value_orders or 0)}</td>" \
+                         f"<td style=\"text-align: right !important;\">{'{:20,.0f}'.format(detail.value_to_upper or 0)}</td>" \
+                         f"<td>{detail.old_card_rank_id.name}</td>" \
+                         f"<td>{detail.new_card_rank_id.name}</td></tr>"
+            value = f"<div class=\"row\">" \
+                    f"<div class=\"col-2\"><b>Hạng:</b></div><div class=\"col-10\">{line.card_rank_id.name}</div>" \
+                    f"<div class=\"col-2\"><b>Doanh số tích lũy:</b></div><div class=\"col-10\">{'{:20,.0f}'.format(line.accumulated_sales or 0)}</div>" \
+                    f"<div class=\"col-2\"><b>Ngày mua gần nhất:</b></div>" \
+                    f"<div class=\"col-10\">{line.last_order_date and line.last_order_date.astimezone(pytz.timezone(self.env.user.tz)).strftime('%d/%m/%Y') or ''}</div></div>" \
+                    f"<br/><table class=\"table table-bordered\">" \
+                    f"<tr style=\"text-align: center; background: #031d74c7; color: #ffffff;\"><th colspan=\"6\">Lịch sử nâng hạng</th></tr>" \
+                    f"<tr style=\"text-align: center; background: #031d74c7; color: #ffffff;\">" \
+                    f"<th>Đơn hàng</th><th>Ngày mua hàng</th><th>Giá trị đơn hàng</th><th>Giá trị xét hạng</th><th>Hạng hiện tại</th><th>Hạng mới</th></tr>" \
+                    f"{_detail}</table>"
+            res.update({
+                f'{line.brand_id.code}-{str(line.customer_id.id)}': value,
+            })
+        return res
+
 
 class PartnerCardRankLine(models.Model):
     _name = 'partner.card.rank.line'
--- addons/custom/forlife_customer_card_rank/models/res_partner.py
+++ addons/custom/forlife_customer_card_rank/models/res_partner.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models, _
-from odoo.exceptions import ValidationError
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     card_rank_ids = fields.One2many('partner.card.rank', inverse_name='customer_id', string='Card Rank')
+    card_rank_format = fields.Html('Card Rank Format', compute='_compute_card_rank')
+    card_rank_tokyolife = fields.Html('Card Rank TokyoLife', compute='_compute_card_rank')
+
+    def _compute_card_rank(self):
+        for line in self:
+            data = line.card_rank_ids.generate_card_rank_data()
+            line.card_rank_format = data.get(f'{self.env.ref("forlife_point_of_sale.brand_format").code}-{str(line.id)}', 'No data')
+            line.card_rank_tokyolife = data.get(f'{self.env.ref("forlife_point_of_sale.brand_tokyolife").code}-{str(line.id)}', 'No data')
+
