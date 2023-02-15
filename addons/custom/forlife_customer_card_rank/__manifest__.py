020a4c146e3ab1deeb80540229bd751659c2fb5e
Daihv <daihvls96@gmail.com>
2023-02-15 15:34:10 +0700

    [IMP] forlife_customer_card_rank - update code

--- addons/custom/forlife_customer_card_rank/__manifest__.py
+++ addons/custom/forlife_customer_card_rank/__manifest__.py
@@ -32,6 +32,7 @@
         'views/member_card_views.xml',
         'views/partner_card_rank_views.xml',
         'views/res_partner_views.xml',
+        'views/pos_order_views.xml',
         'views/menuitem.xml',
     ]
 }
