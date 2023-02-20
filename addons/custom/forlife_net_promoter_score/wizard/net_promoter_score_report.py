52e3c05e5f3a0c385507ce29dc32b69765b31af7
Daihv <daihvls96@gmail.com>
2023-02-20 16:12:20 +0700

    [ADD] forlife_net_promoter_score - add onchange feature

--- addons/custom/forlife_net_promoter_score/wizard/net_promoter_score_report.py
+++ addons/custom/forlife_net_promoter_score/wizard/net_promoter_score_report.py
@@ -35,6 +35,17 @@ class NetPromoterScoreReport(models.TransientModel):
     number_of_record = fields.Char()
     record_by_page = fields.Json()
 
+    @api.onchange('record_per_page', 'from_date', 'to_date', 'min_point', 'max_point', 'brand_ids', 'customer_code', 'invoice_number')
+    def onchange_record_per_page(self):
+        if self.from_date and self.to_date and self.brand_ids:
+            values = self.with_context(pop_colum=True, onchange=True).btn_search()
+            self.view_report = values.get('view_report')
+            self.record_per_page = values.get('record_per_page', self.record_per_page)
+            self.number_of_page = values.get('number_of_page')
+            self.current_page = values.get('current_page')
+            self.number_of_record = values.get('number_of_record')
+            self.record_by_page = values.get('record_by_page')
+
     def get_view_header(self):
         header = copy.copy(REPORT_HEADER)
         for index in POP_INDEX_IN_FORM_REPORT:
@@ -42,8 +53,9 @@ class NetPromoterScoreReport(models.TransientModel):
         return [f'<table class="table table-bordered"><tr style="text-align: center; background: #017e84; color: #ffffff;"><th>{"</th><th>".join(header)}</th></tr>', '</table>']
 
     def btn_search(self):
-        self._check_record_per_page()
-        self.generate_data(self.filter_data([]))
+        values = dict(self._check_record_per_page())
+        result, values = self.filter_data([], values)
+        return self.generate_data(result, values)
 
     def btn_reset_searching(self):
         header = self.get_view_header()
@@ -68,27 +80,25 @@ class NetPromoterScoreReport(models.TransientModel):
         self.load_new_page(-1)
 
     def load_new_page(self, types):
+        values = {}
         page = ((self.current_page or self.number_of_page) + types) % self.number_of_page
         page_data = self.record_by_page.get(str(page), ['', []])
         record_per_page = self.record_by_page.get('record_per_page')
         if self.record_per_page != record_per_page:
-            self.record_per_page = record_per_page
-        self.number_of_record = page_data[0]
-        self.current_page = page
-        self.generate_data(self.filter_data([('id', 'in', page_data[1])]))
+            values.update({'record_per_page': record_per_page})
+        values.update({'number_of_record': page_data[0], 'current_page': page})
+        result, values = self.filter_data([('id', 'in', page_data[1])], values)
+        self.generate_data(result, values)
 
-    def filter_data(self, domain):
+    def filter_data(self, domain, values):
         if not self._context.get('paging', False):
-            domain = ['&', '&', ('status', '=', '1'), ('point', '>=', self.min_point), ('point', '<=', self.max_point)]
+            domain = ['&', '&', '&', ('status', '=', '1'), ('point', '>=', self.min_point), ('point', '<=', self.max_point),('brand', 'in', self.brand_ids.mapped('code'))]
             if self.from_date:
                 domain.insert(0, '&')
                 domain += [('invoice_date', '>=', self.from_date)]
             if self.to_date:
                 domain.insert(0, '&')
                 domain += [('invoice_date', '<', self.to_date + timedelta(days=1))]
-            if self.brand_ids:
-                domain.insert(0, '&')
-                domain += [('brand', 'in', self.brand_ids.mapped('code'))]
             if self.customer_code:
                 cus_code_domain = [('customer_code', 'like', i.strip()) for i in self.customer_code.split(',')]
                 domain.insert(0, '&')
@@ -101,34 +111,40 @@ class NetPromoterScoreReport(models.TransientModel):
                 domain += inv_num_domain
         records = self.env['forlife.comment'].search(domain)
         if self._context.get('pop_colum', False) and not self._context.get('paging', False):
-            records = self._check_paging(records)
-        return self.prepare_report_data(records)
+            records, values = self._check_paging(records, values)
+        return self.prepare_report_data(records), values
 
-    def _check_paging(self, records):
+    def _check_paging(self, records, values):
+        values = dict(values)
         total_records = len(records)
-        self.current_page = 0
-        if total_records > self.record_per_page:
-            _return = records[:self.record_per_page]
+        record_per_page = values.get('record_per_page', self.record_per_page)
+        values.update({'current_page': 0})
+        if total_records > record_per_page:
+            _return = records[:record_per_page]
             number_of_page = 0
-            record_by_page = {'record_per_page': self.record_per_page}
+            record_by_page = {'record_per_page': record_per_page}
             while records:
-                record_in_page = records[:min(len(records), self.record_per_page)].ids
-                records = records[min(len(records), self.record_per_page):]
+                record_in_page = records[:min(len(records), record_per_page)].ids
+                records = records[min(len(records), record_per_page):]
                 record_by_page.update({
                     number_of_page: [
-                        f'{number_of_page * self.record_per_page + 1}-{number_of_page * self.record_per_page + len(record_in_page)} / {total_records}',
+                        f'{number_of_page * record_per_page + 1}-{number_of_page * record_per_page + len(record_in_page)} / {total_records}',
                         record_in_page,
                     ]
                 })
                 number_of_page += 1
-            self.number_of_page = number_of_page or 1
-            self.record_by_page = record_by_page
-            self.number_of_record = f'1-{self.record_per_page} / {total_records}'
-            return _return
-        self.number_of_record = '1-{0} / {0}'.format(total_records) if total_records else ''
-        self.number_of_page = 1
-        self.record_by_page = False
-        return records
+            values.update({
+                'number_of_page': number_of_page or 1,
+                'record_by_page': record_by_page,
+                'number_of_record': f'1-{record_per_page} / {total_records}'
+            })
+            return _return, values
+        values.update({
+            'number_of_page': 1,
+            'record_by_page': False,
+            'number_of_record': '1-{0} / {0}'.format(total_records) if total_records else ''
+        })
+        return records, values
 
     def prepare_report_data(self, records):
         data = [REPORT_HEADER] if not self._context.get('pop_colum', False) else []
@@ -145,15 +161,23 @@ class NetPromoterScoreReport(models.TransientModel):
             row += 1
         return data
 
-    def generate_data(self, result):
+    def generate_data(self, result, values):
+        values = dict(values)
         data = ''
         for line in result:
             style = ' style="background: #0a0a0a3d;"' if int(line[0]) % 2 == 0 else ''
             data += f'<tr{style}><td style="text-align: center;">{"</td><td>".join(line)}</td></tr>'
         header = self.get_view_header()
-        self.view_report = header[0] + (data or DATA_NOT_FOUND) + header[1]
+        values.update({
+            'view_report': header[0] + (data or DATA_NOT_FOUND) + header[1],
+        })
+        if not self._context.get('onchange'):
+            self.sudo().write(values)
+        return values
 
     def _check_record_per_page(self):
+        values = {}
         record_per_page = min(max(self.record_per_page, MIN_RECORD_PER_PAGE), MAX_RECORD_PER_PAGE)
         if self.record_per_page != record_per_page:
-            self.record_per_page = record_per_page
+            values.update({'record_per_page': record_per_page})
+        return values
