aba50ba9f36f9bb92dd003098329b650137315e6
ducminh <ducminh>
2023-02-21 16:27:10 +0700

    MFL2201ER-279:edit-amount-tax-andfix-case-change-partner

--- addons/custom/forlife_pos_point_order/static/src/js/models.js
+++ addons/custom/forlife_pos_point_order/static/src/js/models.js
@@ -3,7 +3,8 @@ odoo.define('forlife_pos_point_order.models', function (require) {
 
     var {PosGlobalState, Orderline, Order} = require('point_of_sale.models');
     const Registries = require('point_of_sale.Registries');
-
+    var utils = require('web.utils');
+    var round_pr = utils.round_precision;
     const PointsOrderLine = (Orderline) =>
         class extends Orderline {
             constructor(obj, options) {
@@ -34,6 +35,31 @@ odoo.define('forlife_pos_point_order.models', function (require) {
             get_point() {
                 return this.point;
             }
+
+            get_price_point_tax(){
+                return this.get_all_prices_of_point().total_point_after_Tax;
+            }
+
+
+            get_all_prices_of_point(qty = 1){
+                var pointOfline = this.point ? parseInt(-this.point) : 0;
+                var tax_point = 0;
+
+                var product =  this.get_product();
+                var taxes_ids = this.tax_ids || product.taxes_id;
+                taxes_ids = _.filter(taxes_ids, t => t in this.pos.taxes_by_id);
+                var product_taxes = this.pos.get_taxes_after_fp(taxes_ids, this.order.fiscal_position);
+
+                var all_taxes_of_point = this.compute_all(product_taxes, pointOfline, qty, this.pos.currency.rounding);
+                _(all_taxes_of_point.taxes).each(function(tax) {
+                    tax_point += tax.amount;
+                });
+
+                return {
+                    "total_point_after_Tax": parseInt(pointOfline) - tax_point
+                };
+            }
+
         };
     const PointsOrder = (Order) =>
         class extends Order {
@@ -52,6 +78,10 @@ odoo.define('forlife_pos_point_order.models', function (require) {
 
             export_as_JSON() {
                 const json = super.export_as_JSON(...arguments);
+                var total = this.get_total_with_tax();
+                var totalWithoutTax = this.get_total_without_tax() - this.get_total_point_tax();
+                var taxAmount = total - totalWithoutTax;
+                json.amount_tax = taxAmount;
                 return json;
             }
             get_total_with_tax() {
@@ -63,6 +93,12 @@ odoo.define('forlife_pos_point_order.models', function (require) {
                     }
                 }
                 return total + vals;
+            }
+
+            get_total_point_tax() {
+                return round_pr(this.orderlines.reduce((function(sum, orderLine) {
+                    return sum + orderLine.get_price_point_tax();
+                }), 0), this.pos.currency.rounding);
             }
 
     }
