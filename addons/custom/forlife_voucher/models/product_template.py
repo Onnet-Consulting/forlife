a0484ee4eb07a3eb144f8d8ace7b13d527fa6d03
ducminh <ducminh>
2023-02-24 09:05:14 +0700

    [FIX]-currency

--- addons/custom/forlife_voucher/models/product_template.py
+++ addons/custom/forlife_voucher/models/product_template.py
@@ -4,16 +4,4 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     voucher = fields.Boolean('Voucher')
-    program_voucher_id = fields.Many2one('program.voucher', compute='compute_program', inverse='program_inverse')
-    program_voucher_ids = fields.One2many('program.voucher', 'product_id')
-
-    @api.depends('program_voucher_ids')
-    def compute_program(self):
-        if len(self.program_voucher_ids) > 0:
-            self.program_voucher_id = self.program_voucher_ids[0]
-
-    def program_inverse(self):
-        if len(self.program_voucher_ids) > 0:
-            program = self.env['program.voucher'].browse(self.program_voucher_ids[0].id)
-            program.product_id = False
-        self.program_voucher_id.product_id = self
+    program_voucher_id = fields.Many2one('program.voucher',)
