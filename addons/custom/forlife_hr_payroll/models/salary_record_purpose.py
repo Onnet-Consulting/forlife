4a8aa45be8d06087ff76b3fc87bd337f6506750b
xmars <xtruongptit@gmail.com>
2023-03-03 15:14:02 +0700

    [FIX] forlifei_hr_payroll - filter invalid account move line

--- addons/custom/forlife_hr_payroll/models/salary_record_purpose.py
+++ addons/custom/forlife_hr_payroll/models/salary_record_purpose.py
@@ -9,6 +9,19 @@ class SalaryRecordPurpose(models.Model):
 
     code = fields.Char(string='Code', required=True)
     name = fields.Char(string='Name', required=True)
+    display_name = fields.Char(compute='_compute_display_name')
+
+    @api.depends('code', 'name')
+    def _compute_display_name(self):
+        for rec in self:
+            rec.display_name = f"[{rec.code}] {rec.name}"
+
+    def name_get(self):
+        result = []
+        for record in self:
+            name = f"[{record.code}] {record.name}"
+            result.append((record.id, name))
+        return result
 
     _sql_constraints = [
         ('unique_code', 'UNIQUE(code)', 'Only one code occurrence by salary record purpose')
