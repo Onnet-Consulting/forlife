a4564514e348c81792b9c4f7e531047e30edc188
Nguyên Anh <tuuh@viettel.com.vn>
2023-03-06 17:21:34 +0700

    [FIX] cập nhật thông tin về quantity phần chịu giá

--- addons/custom/forlife_voucher/models/sale_order.py
+++ addons/custom/forlife_voucher/models/sale_order.py
@@ -15,17 +15,22 @@ class SaleOrder(models.Model):
         for order in self:
             invoice_line_vals = []
             for line in order.order_line:
-                if not line.product_id.program_voucher_id or line.product_id.program_voucher_id.purpose_id.ref != 'B' or line.price_unit == line.product_id.price:
-                    continue
                 if line.product_id.price - line.price_unit <= 0:
                     continue
                 if not line.product_id.categ_id.property_price_account_id:
                     continue
+                imei = []
+                move_line_ids = self.env['stock.move.line'].search([('move_id.sale_line_id', '=', line.id)])
+                for move_line_id in move_line_ids:
+                    imei.append(move_line_id.lot_id.name)
+                quantity = self.env['voucher.voucher'].search_count(
+                    [('sale_id', '=', self.id), ('purpose_id.ref', '=ilike', 'B'), ('name', 'in', imei)])
+
                 invoice_line_vals.append((0, 0, {
                     # 'display_type': line.display_type or 'line_section',
                     'sequence': line.sequence,
                     'account_id': line.product_id.categ_id.property_price_account_id.id,
-                    'quantity': line.qty_delivered,
+                    'quantity': quantity,
                     # 'sale_line_ids': [Command.link(line.id)],
                     'price_unit': line.product_id.price - line.price_unit,
                     'is_downpayment': line.is_downpayment,
