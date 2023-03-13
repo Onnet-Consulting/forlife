01e01ddcf47df135fe3e2c3d64bd0fca02e4ff62
Daihv <daihvls96@gmail.com>
2023-03-13 09:27:59 +0700

    [FIX] forlife_customer_card_rank: remove field

--- addons/custom/forlife_customer_card_rank/models/pos_order_line_discount_details.py
+++ addons/custom/forlife_customer_card_rank/models/pos_order_line_discount_details.py
@@ -4,8 +4,6 @@ from odoo import api, fields, models
 class PosOlDiscountDetails(models.Model):
     _inherit = 'pos.order.line.discount.details'
 
-    card_rank_program_id = fields.Many2one('member.card', related='pos_order_line_id.order_id.card_rank_program_id')
-
     def get_money_reduced(self):
         if self.type == 'card':
             return - (self.recipe * self.listed_price / 100) * self.pos_order_line_id.qty
@@ -13,6 +11,6 @@ class PosOlDiscountDetails(models.Model):
 
     def get_name(self):
         if self.type == 'card':
-            return self.card_rank_program_id.name
+            return self.pos_order_line_id.order_id.card_rank_program_id.name
         return super().get_name()
     
