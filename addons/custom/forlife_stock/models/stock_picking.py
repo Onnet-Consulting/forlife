e95dfef07f63412826645201f32603e2e44caecf
Nguyên Anh <tuuh@viettel.com.vn>
2023-03-30 10:28:48 +0700

    update code

--- addons/custom/forlife_stock/models/stock_picking.py
+++ addons/custom/forlife_stock/models/stock_picking.py
@@ -1,5 +1,41 @@
 from odoo import api, fields, models
 from odoo.tools.float_utils import float_compare, float_is_zero, float_round
+from odoo.addons.stock.models.stock_picking import Picking as InheritPicking
+
+
+def _action_done(self):
+    """Call `_action_done` on the `stock.move` of the `stock.picking` in `self`.
+    This method makes sure every `stock.move.line` is linked to a `stock.move` by either
+    linking them to an existing one or a newly created one.
+
+    If the context key `cancel_backorder` is present, backorders won't be created.
+
+    :return: True
+    :rtype: bool
+    """
+    self._check_company()
+
+    todo_moves = self.move_ids.filtered(
+        lambda self: self.state in ['draft', 'waiting', 'partially_available', 'assigned', 'confirmed'])
+    for picking in self:
+        if picking.owner_id:
+            picking.move_ids.write({'restrict_partner_id': picking.owner_id.id})
+            picking.move_line_ids.write({'owner_id': picking.owner_id.id})
+    todo_moves._action_done(cancel_backorder=self.env.context.get('cancel_backorder'))
+    # edit here: remove update date_done
+    # self.write({'date_done': fields.Datetime.now(), 'priority': '0'})
+    self.write({'priority': '0'})
+
+    # if incoming/internal moves make other confirmed/partially_available moves available, assign them
+    done_incoming_moves = self.filtered(lambda p: p.picking_type_id.code in ('incoming', 'internal')).move_ids.filtered(
+        lambda m: m.state == 'done')
+    done_incoming_moves._trigger_assign()
+
+    self._send_confirmation_email()
+    return True
+
+
+InheritPicking._action_done = _action_done
 
 
 class StockPicking(models.Model):
@@ -38,7 +74,7 @@ class StockPicking(models.Model):
     def _action_done(self):
         old_date_done = self.date_done
         res = super(StockPicking, self)._action_done()
-        if old_date_done:
+        if old_date_done != self.date_done:
             self.date_done = old_date_done
         return res
 
