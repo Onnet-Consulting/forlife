eed6bb94a23ce64a0cdac8a07346c191903b4b07
XMars <xtruongptit@gmail.com>
2023-03-30 09:25:51 +0700

    [FIX] forlife_bravo_integration - add uom.uom

--- addons/custom/forlife_bravo_integration/models/__init__.py
+++ addons/custom/forlife_bravo_integration/models/__init__.py
@@ -9,4 +9,4 @@ from . import hr_employee
 from . import sale_order
 from . import purchase_order
 from . import product_category
-from . import account_tax
+from . import uom_uom
--- addons/custom/forlife_bravo_integration/models/account_tax.py
+++ addons/custom/forlife_bravo_integration/models/account_tax.py
@@ -3,8 +3,6 @@
 from odoo import api, fields, models, _
 from ..fields import *
 
+
 class AccountTax(models.Model):
     _inherit = 'account.tax'
-
-
-    br_1 = fields.BravoCharField()
