65500c9d279b4adf90dce694de36865562ab2c42
XMars <xtruongptit@gmail.com>
2023-04-07 10:21:21 +0700

    [FIX] forlife_bravo_integration - fix warehouse code

--- addons/custom/forlife_bravo_integration/models/stock_warehouse.py
+++ addons/custom/forlife_bravo_integration/models/stock_warehouse.py
@@ -9,6 +9,6 @@ class StockWarehouse(models.Model):
     _inherit = ['stock.warehouse', 'bravo.model']
     _bravo_table = 'B20Warehouse'
 
-    br1 = BravoCharField(odoo_name='whs_code', bravo_name='Code', identity=True)
+    br1 = BravoCharField(odoo_name='code', bravo_name='Code', identity=True)
     br2 = BravoCharField(odoo_name='name', bravo_name='Name')
     br3 = BravoMany2oneField('res.company', odoo_name='company_id', bravo_name='CompanyCode', field_detail='code')
