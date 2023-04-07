0e331809fe2495ab9656083b598f09b1fe6d2a05
XMars <xtruongptit@gmail.com>
2023-04-07 08:14:21 +0700

    [FIX] forlife_bravo_integration - fix currency rate

--- addons/custom/forlife_bravo_integration/models/res_currency_rate.py
+++ addons/custom/forlife_bravo_integration/models/res_currency_rate.py
@@ -8,7 +8,7 @@ class ResCurrencyRate(models.Model):
     _name = 'res.currency.rate'
     _inherit = ['res.currency.rate', 'bravo.model']
 
-    br_1 = BravoMany2oneField('res.currency', odoo_name='currency_id', bravo_name='CurrencyCode')
+    br_1 = BravoMany2oneField('res.currency', odoo_name='currency_id', bravo_name='CurrencyCode', field_detail='code')
     br_2 = BravoCharField(odoo_name='name', bravo_name='StartDate')
     br_3 = BravoDecimalField(odoo_name='inverse_company_rate', bravo_name='BuyingExchangeRate')
 
