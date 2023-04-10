91271d53a08f3cc7fbc99f6ae8184395d52366bc
XMars <xtruongptit@gmail.com>
2023-04-10 13:58:19 +0700

    [FIX] forlife_bravo_integration - update stock move data

--- addons/custom/forlife_bravo_integration/models/stock_move.py
+++ addons/custom/forlife_bravo_integration/models/stock_move.py
@@ -13,16 +13,26 @@ class StockMove(models.Model):
             pass
         return res
 
-    def insert_purchase_picking_data(self):
+    def get_purchase_picking_data(self):
         bravo_table = 'B30AccDocPurchase'
         moves = self.filtered(lambda m: m.purchase_line_id and m.account_move_ids)
-        for move in moves:
+        for idx,move in enumerate(moves, start=1):
             picking = move.picking_id
             picking_data = picking.read(['id', 'name', 'date_done'])
-            picking_partner_data = picking.partner_id.read(['name', 'ref', 'contact_address_complete'])
+            picking_partner= picking.partner_id
             account_move = move.account_move_ids[0]
             bravo_data = {
                 'CompanyCode': picking.company_id.code,
-                'Stt': picking.id,
+                'Stt': picking_data.id,
+                'DocCode': 'NK' if picking_partner.group_id == self.env.ref('forlife_pos_app_member.partner_group_1') else 'NM',
+                'DocNo': picking.name,
+                'DocDate': move.date,
+                'CustomerCode': picking_partner.ref,
+                'CustomerName': picking_partner.name,
+                'Address': picking_partner.contact_address_complete,
+                'EmployeeCode': picking.user_id.employee_id.code,
+                'BuiltinOrder': idx
             }
+            for acl in account_move:
+                pass
 
