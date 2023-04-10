29053c7d844bc9f774f130f28add84c869189a0b
XMars <xtruongptit@gmail.com>
2023-04-10 16:41:19 +0700

    [IMP] forlife_bravo_integration - update purchase picking data

--- addons/custom/forlife_bravo_integration/models/stock_move.py
+++ addons/custom/forlife_bravo_integration/models/stock_move.py
@@ -2,29 +2,47 @@
 
 from odoo import api, fields, models
 
+CONTEXT_MOVE_ACTION = 'bravo_move_action'
+
 
 class StockMove(models.Model):
     _name = 'stock.move'
-    _inherit = ['stock.move', 'mssql.server']
+    _inherit = ['stock.move', 'bravo.model.insert.action']
 
     def _action_done(self, cancel_backorder=False):
         res = super()._action_done(cancel_backorder=cancel_backorder)
-        for move in self.filtered(lambda m: m.state == 'done'):
-            pass
+        moves = self.filtered(lambda m: m.state == 'done')
+        insert_queries = moves.bravo_get_insert_sql()
+        self.env[self._name].sudo().with_delay().bravo_insert(insert_queries)
         return res
 
+    @api.model
+    def bravo_get_table(self):
+        move_type = self.env.context.get(CONTEXT_MOVE_ACTION)
+        bravo_table = 'DEFAULT'
+        if move_type == 'purchase_picking':
+            bravo_table = 'B30AccDocPurchase'
+        return bravo_table
+
+    @api.model
+    def bravo_get_default_insert_value(self):
+        return {
+            'PushDate': "SYSDATETIMEOFFSET() AT TIME ZONE 'SE Asia Standard Time'",
+        }
+
     def get_purchase_picking_data(self):
-        bravo_table = 'B30AccDocPurchase'
         moves = self.filtered(lambda m: m.purchase_line_id and m.account_move_ids)
-        for idx,move in enumerate(moves, start=1):
+        bravo_column_names = []
+        moves_data = []
+        for idx, move in enumerate(moves, start=1):
             picking = move.picking_id
-            picking_data = picking.read(['id', 'name', 'date_done'])
-            picking_partner= picking.partner_id
+            picking_partner = picking.partner_id
             account_move = move.account_move_ids[0]
             bravo_data = {
                 'CompanyCode': picking.company_id.code,
-                'Stt': picking_data.id,
-                'DocCode': 'NK' if picking_partner.group_id == self.env.ref('forlife_pos_app_member.partner_group_1') else 'NM',
+                'Stt': picking.id,
+                'DocCode': 'NK' if picking_partner.group_id == self.env.ref(
+                    'forlife_pos_app_member.partner_group_1') else 'NM',
                 'DocNo': picking.name,
                 'DocDate': move.date,
                 'CustomerCode': picking_partner.ref,
@@ -33,6 +51,44 @@ class StockMove(models.Model):
                 'EmployeeCode': picking.user_id.employee_id.code,
                 'BuiltinOrder': idx
             }
-            for acl in account_move:
-                pass
+            bravo_data.update({
+                'Quantity9': move.quantity_done,
+                'DocNo_PO': picking.origin,
+                'WarehouseCode': move.location_dest_id.warehouse_id.code,
+                'RowId': move.id,
+                'DebitAccount': None,
+                'CreditAccount': None,
+            })
+            for acl in account_move.line_ids:
+                if acl.debit > 0:
+                    bravo_data.update({
+                        'DebitAccount': acl.account_id.code
+                    })
+                if acl.credit > 0:
+                    bravo_data.update({
+                        'CreditAccount': acl.account_id.code
+                    })
 
+            moves_data.append(bravo_data)
+            if bravo_column_names:
+                continue
+            bravo_column_names = list(bravo_data.keys())
+        return bravo_column_names, moves_data
+
+    def bravo_get_insert_values(self):
+        move_type = self.env.context.get(CONTEXT_MOVE_ACTION)
+        if move_type == 'purchase_picking':
+            return self.get_purchase_picking_data()
+        return [], []
+
+    def bravo_get_insert_sql_by_move_action(self):
+        queries = []
+        purchase_picking_query = self.with_context(**{CONTEXT_MOVE_ACTION: 'purchase_picking'}).bravo_get_insert_sql()
+        if purchase_picking_query:
+            queries.extend(purchase_picking_query)
+        return queries
+
+    def bravo_get_insert_sql(self):
+        if self.env.context.get(CONTEXT_MOVE_ACTION):
+            return super().bravo_get_insert_sql()
+        return self.bravo_get_insert_sql_by_move_action()
