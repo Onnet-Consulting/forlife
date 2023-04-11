72b1605e8f413c32ba17ff6a848d856b09826836
Lê Duy Mạnh (Onnet) <manh.le@arrowhitech.com>
2023-04-11 09:12:43 +0700

    [IMP][MFL2201ER-528] chinh sua chuc nang doi tra

--- addons/custom/forlife_pos_product_change_refund/models/product_product.py
+++ addons/custom/forlife_pos_product_change_refund/models/product_product.py
@@ -4,5 +4,11 @@ from odoo import api, fields, models
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
-    number_days_change_refund = fields.Integer('Number days change/refurd')
+    # number_days_change_refund = fields.Integer('Number days change/refurd')
 
+    @api.model
+    def get_product_auto(self):
+        product_id = self.search([('is_product_auto', '=', True)], limit=1)
+        if product_id:
+            return product_id.id
+        return False
