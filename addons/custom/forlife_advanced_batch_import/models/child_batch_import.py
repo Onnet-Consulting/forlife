d4dbd1c99e18e622ae8990cce7eb871d1824849e
Dao Thang <thangdv@icheck.vn>
2023-04-11 23:32:09 +0700

    MFL2201ER-586: add new logic pass import valid records, skip error

--- addons/custom/forlife_advanced_batch_import/models/child_batch_import.py
+++ addons/custom/forlife_advanced_batch_import/models/child_batch_import.py
@@ -16,6 +16,17 @@ class ChildBatchImport(models.Model):
     skip = fields.Integer(string='Skip')
     log = fields.Text(string="Log")
     status = fields.Selection([('draft', 'Draft'), ('processing', 'Processing'), ('done', 'Done'), ('error', 'Error'), ('cancel', 'Cancel')], string='Status', default='draft')
+    complete_records = fields.Integer(string="Complete Records")
+    error_rows = fields.Text(string="Error Rows")
+    progress_bar = fields.Float('Progress Done (%)', digits=(16, 2), compute='_compute_progress_bar')
+
+    @api.depends('complete_records', 'parent_batch_import_id', 'parent_batch_import_id.limit')
+    def _compute_progress_bar(self):
+        for rec in self:
+            complete = 0
+            if rec.complete_records and rec.parent_batch_import_id and rec.parent_batch_import_id.limit and rec.complete_records <= int(rec.parent_batch_import_id.limit):
+                complete = (rec.complete_records / int(rec.parent_batch_import_id.limit)) * 100
+            rec.progress_bar = complete
 
     def make_queue_job(self):
         for rec in self:
@@ -42,11 +53,22 @@ class ChildBatchImport(models.Model):
                                                                options=options,
                                                                dryrun=rec.parent_batch_import_id.dryrun)
                 if result.get('ids') and len(result.get('messages')) == 0:
-                    rec.status = 'done'
-                    rec.log = json.dumps(result, ensure_ascii=False)
+                    rec.write({
+                        'status': 'done',
+                        'log': json.dumps(result, ensure_ascii=False),
+
+                    })
+
                 else:
                     rec.status = 'error'
                     rec.log = json.dumps(result, ensure_ascii=False)
+                index_for_header = 1 if options.get('has_headers') else 0
+                error_rows = [int(row.get('record')) + index_for_header for row in (result.get('messages') if result.get('messages') else [])]
+                if error_rows:
+                    rec.write({
+                        'error_rows': json.dumps(error_rows),
+                        'complete_records': rec.parent_batch_import_id.limit - len(result.get('messages')) if (rec.parent_batch_import_id and rec.parent_batch_import_id.limit) else 0
+                    })
             except Exception as e:
                 rec.status = 'error'
                 rec.log = str(e)
