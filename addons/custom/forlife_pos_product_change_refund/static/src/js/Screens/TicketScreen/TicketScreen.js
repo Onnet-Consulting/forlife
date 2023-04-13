99e3773143422155713018d297fed5c2539773aa
Lê Duy Mạnh (Onnet) <manh.le@arrowhitech.com>
2023-04-13 17:04:58 +0700

    [IMP][MFL2201ER-528] chinh sua chuc nang doi tra

---
+++ addons/custom/forlife_pos_product_change_refund/static/src/js/Screens/TicketScreen/TicketScreen.js
@@ -0,0 +1,39 @@
+odoo.define('forlife_pos_product_change_refund.TicketScreen', function (require) {
+    'use strict';
+
+    const { Order } = require('point_of_sale.models');
+    const Registries = require('point_of_sale.Registries');
+    const TicketScreen = require('point_of_sale.TicketScreen');
+
+    const TicketScreenChangeRefund = TicketScreen => class extends TicketScreen {
+
+        async _removeHandleChangeRefund(order) {
+            for (const line of order.get_orderlines()) {
+                if (line.handle_change_refund_id) {
+                    var args = {};
+                    args.id = line.handle_change_refund_id;
+                    await this.rpc({
+                        model: 'handle.change.refund',
+                        method: 'cancel_rc_handle_change_refund',
+                        args: [args],
+                    })
+                }
+            }
+        }
+
+        async _onDeleteOrder({ detail: order }) {
+            await super._onDeleteOrder(...arguments);
+            const new_orders = this.env.pos.get_order_list();
+            for (const new_order of new_orders) {
+                if (new_order === order) {
+                    return;
+                }
+            }
+            this._removeHandleChangeRefund(order);
+        }
+    };
+
+    Registries.Component.extend(TicketScreen, TicketScreenChangeRefund);
+
+    return TicketScreenChangeRefund;
+});
