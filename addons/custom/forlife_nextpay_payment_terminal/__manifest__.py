312f3a46060d79dd0e482390c02f2e78deb05c88
XMars <xtruongptit@gmail.com>
2023-04-18 10:54:04 +0700

    [ADD] forlife_nextpay_payment_terminal,forlife_vnpay_payment_terminal - add modules

---
+++ addons/custom/forlife_nextpay_payment_terminal/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "NextPay payment terminal",
+
+    'summary': """""",
+
+    'description': """
+    """,
+
+    'author': "ForLife",
+    'website': "",
+    "license": "LGPL-3",
+
+    'category': 'Generic Modules',
+    'version': '16.0.1.0.0',
+
+    'depends': ['point_of_sale'],
+    'data': [
+    ],
+}
