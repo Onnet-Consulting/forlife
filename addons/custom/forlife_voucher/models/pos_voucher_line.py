dd85db9af02f1f780cee09bf676746f7ca357952
Lê Duy Mạnh (Onnet) <manh.le@arrowhitech.com>
2023-04-22 12:10:11 +0700

    [FIX][MFL2201ER-619] dich, sua thong bao trong doi tra

--- addons/custom/forlife_voucher/models/pos_voucher_line.py
+++ addons/custom/forlife_voucher/models/pos_voucher_line.py
@@ -30,3 +30,15 @@ class PosVoucherLine(models.Model):
     product_voucher_name = fields.Char('Product Voucher Name')
     derpartment_name = fields.Char('Derpartment Name')
 
+    def _export_for_ui(self, voucher):
+        return {
+            'voucher_id': voucher.voucher_id.id,
+            'voucher_name': voucher.voucher_id.name,
+            'price_residual': voucher.price_residual,
+            'price_used': voucher.price_used,
+            'type': voucher.type,
+        }
+
+    def export_for_ui(self):
+        return self.mapped(self._export_for_ui) if self else []
+
