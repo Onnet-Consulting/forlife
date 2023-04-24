90c1caececdf213d742424f326a1321f805d87ad
minhduc <minhduc_onnet>
2023-04-25 00:56:43 +0700

    delete-contrains

--- addons/custom/forlife_voucher/models/product_program_import.py
+++ addons/custom/forlife_voucher/models/product_program_import.py
@@ -9,10 +9,6 @@ class ProductProgramImport(models.Model):
     product_id = fields.Many2one('product.product', string='Product')
     barcode = fields.Char('Mã vạch', related='product_id.barcode')
 
-    _sql_constraints = [
-        ('unique_product_id', 'UNIQUE(product_id, program_vocher_id)', 'Product barcode must be unique!')
-    ]
-
     @api.model_create_multi
     def create(self, vals_list):
         for idx, line in enumerate(vals_list):
