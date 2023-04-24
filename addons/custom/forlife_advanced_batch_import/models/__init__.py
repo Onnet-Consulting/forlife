a75c9e75cf6a99984ff757e23e36eac14e2a82fa
Hoàng Trọng Tài (Onnet) <tai.hoang@onnet.vn>
2023-04-24 02:44:21 +0000

    Update addons/custom/forlife_advanced_batch_import/models/__init__.py

--- addons/custom/forlife_advanced_batch_import/models/__init__.py
+++ addons/custom/forlife_advanced_batch_import/models/__init__.py
@@ -5,4 +5,4 @@ from . import child_batch_import
 from . import models
 from . import base_import
 from . import product_attribute
-from . import stock_valuation_layer
+# from . import stock_valuation_layer
