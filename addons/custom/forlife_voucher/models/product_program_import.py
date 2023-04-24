86d0a7344e3d85afa2cc9655472e9c8d3cf23ba5
minhduc <minhduc_onnet>
2023-04-25 01:00:41 +0700

    delete-contrains

--- addons/custom/forlife_voucher/models/product_program_import.py
+++ addons/custom/forlife_voucher/models/product_program_import.py
@@ -5,19 +5,19 @@ class ProductProgramImport(models.Model):
 
     _description = 'Sản phẩm áp dụng (Customize luồng import)'
 
-    program_vocher_id = fields.Many2one('program.voucher','Program Voucher')
-    product_id = fields.Many2one('product.product', string='Product')
-    barcode = fields.Char('Mã vạch', related='product_id.barcode')
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        for idx, line in enumerate(vals_list):
-            if ('active_model' in self._context and self._context.get('active_model')) and ('active_id' in self._context and self._context.get('active_id')):
-                program_vocher = self.env['program.voucher'].sudo().search([('id','=',int(self._context.get('default_program_vocher_id')))])
-                program_vocher.product_apply_ids = [(4, int(vals_list[idx]['product_id']))]
-        return super(ProductProgramImport, self).create(vals_list)
-
-    def unlink(self):
-        for rec in self:
-            rec.program_vocher_id.product_apply_ids = [(3, rec.product_id.id)]
-        return super(ProductProgramImport, self).unlink()
+    # program_vocher_id = fields.Many2one('program.voucher','Program Voucher')
+    # product_id = fields.Many2one('product.product', string='Product')
+    # barcode = fields.Char('Mã vạch', related='product_id.barcode')
+    #
+    # @api.model_create_multi
+    # def create(self, vals_list):
+    #     for idx, line in enumerate(vals_list):
+    #         if ('active_model' in self._context and self._context.get('active_model')) and ('active_id' in self._context and self._context.get('active_id')):
+    #             program_vocher = self.env['program.voucher'].sudo().search([('id','=',int(self._context.get('default_program_vocher_id')))])
+    #             program_vocher.product_apply_ids = [(4, int(vals_list[idx]['product_id']))]
+    #     return super(ProductProgramImport, self).create(vals_list)
+    #
+    # def unlink(self):
+    #     for rec in self:
+    #         rec.program_vocher_id.product_apply_ids = [(3, rec.product_id.id)]
+    #     return super(ProductProgramImport, self).unlink()
