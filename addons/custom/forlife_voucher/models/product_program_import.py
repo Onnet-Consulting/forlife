7f5a106a183af55d13e2bf103724642a6365b1b9
minhduc <minhduc_onnet>
2023-04-25 15:41:15 +0700

    delete-data-model-import

--- addons/custom/forlife_voucher/models/product_program_import.py
+++ addons/custom/forlife_voucher/models/product_program_import.py
@@ -5,26 +5,26 @@ class ProductProgramImport(models.Model):
 
     _description = 'Sản phẩm áp dụng (Customize luồng import)'
 
-    program_vocher_id = fields.Many2one('program.voucher','Program Voucher')
-    product_id = fields.Many2one('product.product', string='Product')
-    barcode = fields.Char('Mã vạch', related='product_id.barcode')
-
-    _sql_constraints = [
-        ('unique_product_id', 'UNIQUE(product_id, program_vocher_id)', 'Product must be unique!')
-    ]
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        for idx, line in enumerate(vals_list):
-            if 'default_program_vocher_id' in self._context and self._context.get('default_program_vocher_id'):
-                id_program_vocher = int(self._context.get('default_program_vocher_id'))
-            else:
-                id_program_vocher = int(vals_list[idx]['program_vocher_id'])
-            program_vocher = self.env['program.voucher'].sudo().search([('id','=',id_program_vocher)])
-            program_vocher.product_apply_ids = [(4, int(vals_list[idx]['product_id']))]
-        return super(ProductProgramImport, self).create(vals_list)
-
-    def unlink(self):
-        for rec in self:
-            rec.program_vocher_id.product_apply_ids = [(3, rec.product_id.id)]
-        return super(ProductProgramImport, self).unlink()
+    # program_vocher_id = fields.Many2one('program.voucher','Program Voucher')
+    # product_id = fields.Many2one('product.product', string='Product')
+    # barcode = fields.Char('Mã vạch', related='product_id.barcode')
+    #
+    # _sql_constraints = [
+    #     ('unique_product_id', 'UNIQUE(product_id, program_vocher_id)', 'Product must be unique!')
+    # ]
+    #
+    # @api.model_create_multi
+    # def create(self, vals_list):
+    #     for idx, line in enumerate(vals_list):
+    #         if 'default_program_vocher_id' in self._context and self._context.get('default_program_vocher_id'):
+    #             id_program_vocher = int(self._context.get('default_program_vocher_id'))
+    #         else:
+    #             id_program_vocher = int(vals_list[idx]['program_vocher_id'])
+    #         program_vocher = self.env['program.voucher'].sudo().search([('id','=',id_program_vocher)])
+    #         program_vocher.product_apply_ids = [(4, int(vals_list[idx]['product_id']))]
+    #     return super(ProductProgramImport, self).create(vals_list)
+    #
+    # def unlink(self):
+    #     for rec in self:
+    #         rec.program_vocher_id.product_apply_ids = [(3, rec.product_id.id)]
+    #     return super(ProductProgramImport, self).unlink()
