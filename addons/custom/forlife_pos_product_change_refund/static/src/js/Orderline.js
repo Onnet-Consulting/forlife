9def8a2ee242cb97b0590df54ff3ab8909b4068d
Lê Duy Mạnh (Onnet) <manh.le@arrowhitech.com>
2023-04-25 14:28:46 +0700

    [FIX][MFL2201ER-619] change caculate point

--- addons/custom/forlife_pos_product_change_refund/static/src/js/Orderline.js
+++ addons/custom/forlife_pos_product_change_refund/static/src/js/Orderline.js
@@ -68,6 +68,10 @@ odoo.define('forlife_pos_product_change_refund.Orderline', function(require) {
                 if (data_update.status === 'approve') {
                     this.props.line.set_unit_price(data_update.price);
                     this.props.line.beStatus = true;
+                    const order = this.env.pos.get_order();
+                    if (order) {
+                        order.approved = true;
+                    }
                 }
                 else {
                     this.props.line.beStatus = false;
