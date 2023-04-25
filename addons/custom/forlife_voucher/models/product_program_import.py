efb5d34be75dcf98d5374b226ba07ca47ca326cc
minhduc <minhduc_onnet>
2023-04-25 11:39:04 +0700

    customizr-batch-import-for-voucher

--- addons/custom/forlife_voucher/models/product_program_import.py
+++ addons/custom/forlife_voucher/models/product_program_import.py
@@ -12,9 +12,9 @@ class ProductProgramImport(models.Model):
     @api.model_create_multi
     def create(self, vals_list):
         for idx, line in enumerate(vals_list):
-            if ('active_model' in self._context and self._context.get('active_model')) and ('active_id' in self._context and self._context.get('active_id')):
-                program_vocher = self.env['program.voucher'].sudo().search([('id','=',int(self._context.get('default_program_vocher_id')))])
-                program_vocher.product_apply_ids = [(4, int(vals_list[idx]['product_id']))]
+            id_program_vocher = int(self._context.get('default_program_vocher_id')) or vals_list[idx]['program_vocher_id']
+            program_vocher = self.env['program.voucher'].sudo().search([('id','=',id_program_vocher)])
+            program_vocher.product_apply_ids = [(4, int(vals_list[idx]['product_id']))]
         return super(ProductProgramImport, self).create(vals_list)
 
     def unlink(self):
