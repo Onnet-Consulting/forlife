029a3fc869cfa9e7841ceab1692378c8948b1907
Hoàng Trọng Tài (Onnet) <tai.hoang@onnet.vn>
2023-05-03 10:13:45 +0000

    Update addons/custom/forlife_pos_promotion/static/src/js/db.js

--- addons/custom/forlife_pos_promotion/static/src/js/db.js
+++ addons/custom/forlife_pos_promotion/static/src/js/db.js
@@ -4,8 +4,8 @@ odoo.define('forlife_pos_promotion.db', function (require) {
     const PosDB = require('point_of_sale.DB');
     PosDB.include({
             get_partner_write_date: function(){
-                var d = new Date()
-                var today = String(d.getFullYear()) + '-' + String(d.getMonth()+1) + '-' + String(d.getDay()) + ' '+ String(d.getHours())+':'+ String(d.getMinutes())+':'+String(d.getSeconds())
+                var d = new Date();
+                var today = String(d.getFullYear()) + '-' + String(d.getMonth()+1) + '-' + String(d.getDay()) + ' 00:00:00';
                 return this.partner_write_date || today;
             },
     });
