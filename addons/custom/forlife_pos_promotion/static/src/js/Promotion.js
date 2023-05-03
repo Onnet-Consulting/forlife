657c3bee464b8ce66492178b4e9e2d1a28dd223f
taiht <taiht>
2023-05-03 14:21:00 +0700

    promotion code percent apply order

--- addons/custom/forlife_pos_promotion/static/src/js/Promotion.js
+++ addons/custom/forlife_pos_promotion/static/src/js/Promotion.js
@@ -822,6 +822,25 @@ const PosPromotionOrder = (Order) => class PosPromotionOrder extends Order {
             count += oddComboNumber;
         }
 
+        if (codeProgram.reward_type == "code_percent" && codeProgram.discount_apply_on == "order" && count > 0) {
+            for (const ol of to_check_order_lines.filter(ol => valid_product_ids.has(ol.product.id))) {
+                to_discount_line_vals.push({
+                    product: ol.product,
+                    quantity:  ol.quantity,
+                    price: ol.price,
+                    isNew: true,
+                    promotion_usage_ids: [...ol.promotion_usage_ids]
+                });
+                ol.quantity = ol.quantity - quantity_combo * min_quantity;
+                ol.quantityStr = field_utils.format.float(ol.quantity, {digits: [69, decimals]});
+                if (ol.key_program && to_apply_lines[ol.key_program]) {
+                    for (let new_line of to_apply_lines[ol.key_program].filter((l)=>l.product.id === ol.product.id)) {
+                        new_line.quantity = ol.quantity;
+                    }
+                }
+            }
+        }
+
         var total_price = to_discount_line_vals.reduce((p, {price, quantity}) => p + price*quantity, 0)
         for (const discount_line_val of to_discount_line_vals) {
             discount_line_val.total_price = total_price;
