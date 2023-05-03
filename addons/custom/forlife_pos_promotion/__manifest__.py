90515aab454a7a21222ea3affc71538ddfd696ff
minhduc <minhduc_onnet>
2023-05-03 13:49:44 +0700

    format-time-today

--- addons/custom/forlife_pos_promotion/__manifest__.py
+++ addons/custom/forlife_pos_promotion/__manifest__.py
@@ -35,6 +35,7 @@
             'forlife_pos_promotion/static/src/js/Popup/CartPromotionPopup.js',
             'forlife_pos_promotion/static/src/js/Popup/RewardSelectionCartPromotionPopup.js',
             'forlife_pos_promotion/static/src/js/Popup/SurpriseRewardPopup.js',
+            'forlife_pos_promotion/static/src/js/db.js',
 
             'forlife_pos_promotion/static/src/xml/ControlButtons/PromotionButton.xml',
             'forlife_pos_promotion/static/src/xml/ControlButtons/EnterCodeButton.xml',
--- addons/custom/forlife_pos_promotion/static/src/js/db.js
+++ addons/custom/forlife_pos_promotion/static/src/js/db.js
@@ -4,8 +4,9 @@ odoo.define('forlife_pos_promotion.db', function (require) {
     const PosDB = require('point_of_sale.DB');
     PosDB.include({
             get_partner_write_date: function(){
-                date = new Date()
-                return this.partner_write_date || "1970-01-01 00:00:00";
+                var d = new Date()
+                var today = String(d.getFullYear()) + '-' + String(d.getMonth()+1) + '-' + String(d.getDay()) + ' '+ String(d.getHours())+':'+ String(d.getMinutes())+':'+String(d.getSeconds())
+                return this.partner_write_date || today;
             },
     });
 });
