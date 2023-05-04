0f57404fb80af23a468ea7362812d5b43f30dc56
XMars <xtruongptit@gmail.com>
2023-05-04 16:52:50 +0700

    [ADD] forlife_pos_print_receipt - add module print pos receipts

---
+++ addons/custom/forlife_pos_print_receipt/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "PoS print receipts",
+
+    'summary': """""",
+
+    'description': """
+    """,
+
+    'author': "ForLife",
+    'website': "",
+    "license": "LGPL-3",
+
+    'category': 'Point of Sales',
+    'version': '16.0.1.0.0',
+
+    'depends': ['point_of_sale'],
+    'auto_install': True,
+
+    'data': [
+    ],
+    'assets': {
+        'point_of_sale.assets': [
+            'forlife_pos_print_receipt/static/src/js/**/*.js',
+            'forlife_pos_print_receipt/static/src/xml/**/*.xml',
+        ],
+    }
+}
