8c3de6166e1a739232bce0f5783b188834d3b4d5
Tai <ckaulenbar1@gmail.com>
2023-05-07 17:21:38 +0700

    update invoice

--- addons/custom/forlife_invoice/models/forlife_product_invoice.py
+++ addons/custom/forlife_invoice/models/forlife_product_invoice.py
@@ -5,4 +5,12 @@ from odoo.exceptions import ValidationError
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    is_check_cost = fields.Boolean(default=False, string='Thuộc chi phí')
+    # is_check_cost = fields.Boolean(default=False, string='Thuộc chi phí')
+    #
+    # @api.depends('detailed_type')
+    # def _compute_is_check_cost(self):
+    #     for rec in self:
+    #         if rec.detailed_type == 'service':
+    #             rec.is_check_cost = True
+
+
