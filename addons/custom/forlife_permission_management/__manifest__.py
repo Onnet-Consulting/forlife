e24f6eba64874a2c9bd783e8f25d2ffd99dc4525
Tungpd-NES <108250170+Tungpd-NES@users.noreply.github.com>
2023-05-09 21:21:52 +0700

    resolve task 663

---
+++ addons/custom/forlife_permission_management/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Forlife Custom',
+    'version': '1.0',
+    'description': '',
+    'summary': '',
+    'author': 'ForLife',
+    'website': '',
+    'license': 'LGPL-3',
+    'category': 'Hidden',
+    'depends': [
+        'base',
+        'product',
+    ],
+    'installable': True,
+    'auto_install': True,
+    'data': [
+        'views/product_product_views.xml',
+        'views/res_users_views.xml',
+    ],
+}
