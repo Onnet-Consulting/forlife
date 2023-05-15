60e2fbfb170f392b390a4f9f03f9ccc73fdddfe6
manhdt <manhdt@aht-asia.com>
2023-05-15 19:15:28 +0700

    tao combo khi mua san pham

--- addons/custom/forlife_product_combo/models/pos_session.py
+++ addons/custom/forlife_product_combo/models/pos_session.py
@@ -8,18 +8,37 @@ class PosSession(models.Model):
     def load_pos_data(self):
         loaded_data = super(PosSession, self).load_pos_data()
         product_combos = self.env['product.combo'].sudo().search([('state', '=', 'in_progress')])
+        product_combo_lines = []
+        for rec in product_combos:
+            product_combolines = self.env['product.combo.line'].sudo().search([('combo_id', '=', rec.id), ('state', '=', 'in_progress')])
+            combo_ids = [{
+                'product_id': cpl.product_id.id,
+                'product_name': cpl.product_id.name,
+                'quantity': cpl.max_quantity,
+            } for cpl in product_combolines]
 
-        product_combos = [{
-            'id': r.id,
-            'code': r.code,
-            'from_date': r.from_date,
-            'to_date': r.to_date,
-            'size_deviation_allowed': r.size_deviation_allowed,
-            'color_deviation_allowed': r.color_deviation_allowed
-        } for r in product_combos]
+            product_combo_list = {
+                'id': rec.id,
+                'code': rec.code,
+                'from_date': rec.from_date,
+                'to_date': rec.to_date,
+                'size_deviation_allowed': rec.size_deviation_allowed,
+                'color_deviation_allowed': rec.color_deviation_allowed,
+                'product_combolines': combo_ids,
+            }
+            product_combo_lines.append(product_combo_list)
+
+        # product_combos = [{
+        #     'id': r.id,
+        #     'code': r.code,
+        #     'from_date': r.from_date,
+        #     'to_date': r.to_date,
+        #     'size_deviation_allowed': r.size_deviation_allowed,
+        #     'color_deviation_allowed': r.color_deviation_allowed
+        # } for r in product_combos]
 
         loaded_data.update({
-            'product_combo': product_combos,
+            'product_combo': product_combo_lines,
         })
         return loaded_data
     def _loader_params_product_product(self):
--- addons/custom/forlife_product_combo/models/product_combo.py
+++ addons/custom/forlife_product_combo/models/product_combo.py
@@ -35,6 +35,9 @@ class ProductCombo(models.Model):
             pr.product_id.write({
                 'combo_id': result.id if vals['state'] in ['in_progress'] else None
             })
+            pr.product_id.product_variant_id.write({
+                'combo_id': self.id if self.state in ['in_progress'] else None
+            })
 
         return result
 
@@ -42,6 +45,9 @@ class ProductCombo(models.Model):
         res = super().write(values)
         for pr in self.combo_product_ids:
             pr.product_id.write({
+                'combo_id': self.id if self.state in ['in_progress'] else None
+            })
+            pr.product_id.product_variant_id.write({
                 'combo_id': self.id if self.state in ['in_progress'] else None
             })
         return res
