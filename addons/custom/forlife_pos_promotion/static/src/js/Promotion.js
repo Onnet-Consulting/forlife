157a5a9589390d678d43efa381302d3b2d027af8
Minh <minh.nguyen@onnet.vn>
2023-05-16 14:39:32 +0700

    MFL2201ER-promotion: Chiết khấu chéo cộng dồn theo giá trị hóa đơn

--- addons/custom/forlife_pos_promotion/static/src/js/Promotion.js
+++ addons/custom/forlife_pos_promotion/static/src/js/Promotion.js
@@ -1421,6 +1421,8 @@ const PosPromotionOrder = (Order) => class PosPromotionOrder extends Order {
 
         let result = [];
         for (let program of cardPrograms) {
+            let max_reward_quantity = program.reward_quantity;
+            let required_order_amount_min = program.order_amount_min;
             if (!this._programIsApplicableAutomatically(program)) {
                 continue
             };
@@ -1438,6 +1440,12 @@ const PosPromotionOrder = (Order) => class PosPromotionOrder extends Order {
             if (program.order_amount_min >0 && program.order_amount_min > amountCheck) {
                 continue;
             };
+            // Tính lũy tuyến cho số lượng phần thưởng
+            if (program.progressive_reward_compute && program.order_amount_min) {
+                let floorNumber = Math.floor(amountCheck/program.order_amount_min);
+                max_reward_quantity = floorNumber * program.reward_quantity;
+                required_order_amount_min = floorNumber * required_order_amount_min;
+            }
             let to_check_products = program.valid_product_ids.size > 0;
             let qty_taken = 0;
             for (const line of orderLines) {
@@ -1463,6 +1471,8 @@ const PosPromotionOrder = (Order) => class PosPromotionOrder extends Order {
             result.push({
                 id: program.id,
                 program: program,
+                max_reward_quantity: max_reward_quantity,
+                required_order_amount_min: required_order_amount_min,
                 voucher_program_id,
                 to_reward_lines,
                 to_discount_lines,
