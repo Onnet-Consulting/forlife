c3560a81962a36aa641c071d9bbde9fd0d6841de
Tungpd-NES <108250170+Tungpd-NES@users.noreply.github.com>
2023-05-16 20:42:27 +0700

    permission menu

--- addons/custom/forlife_permission_management/models/res_users.py
+++ addons/custom/forlife_permission_management/models/res_users.py
@@ -12,4 +12,6 @@ class ResUsers(models.Model):
     brand_default_id = fields.Many2one('res.brand', string='Branch Default')
     department_ids = fields.Many2many('hr.department', string='Department')
     department_default_id = fields.Many2one('hr.department', string='Department Default')
+    team_ids = fields.Many2many('hr.team', string='Team')
+    team_default_id = fields.Many2one('hr.team', string='Team Default')
 
