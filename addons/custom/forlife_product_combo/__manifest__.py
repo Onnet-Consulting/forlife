29d1d201d29beee73bcfbc77045392b4649824df
Tai <ckaulenbar1@gmail.com>
2023-05-20 11:01:58 +0700

    Revert "Merge branch 'beta' into tai-beta-vss"
    This reverts commit 3bef01a103d642531d8759ec53d4a6164c78e5fa, reversing
    changes made to 583fcb9f6a59d18389e4bdc35d8044cf0924cb99.

--- addons/custom/forlife_product_combo/__manifest__.py
+++ addons/custom/forlife_product_combo/__manifest__.py
@@ -15,6 +15,12 @@
     'depends': [
         'point_of_sale',
     ],
+    'assets': {
+        'point_of_sale.assets': [
+            'forlife_product_combo/static/src/js/models.js',
+            'forlife_product_combo/static/src/js/Screens/ProductScreen/ProductScreen.js',
+        ],
+    },
     'data': [
         # Views
         'views/product_combo_views.xml',
--- addons/custom/forlife_product_combo/models/__init__.py
+++ addons/custom/forlife_product_combo/models/__init__.py
@@ -4,3 +4,4 @@ from . import product_combo
 from . import product_combo_line
 from . import pos_session
 from . import product_product
+from . import product_template
