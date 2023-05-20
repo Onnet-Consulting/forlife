5e94a68c64ce52cfb51dd5c5ef28d608ae887c7b
Tai <ckaulenbar1@gmail.com>
2023-05-20 14:27:46 +0700

    Revert "Merge branch 'beta' into beta-vss"
    This reverts commit 7648f93b5e2fc85cf220251afd05a51c9d25c842, reversing
    changes made to ebaa9875c9bc5f2c141cf14df7373297b2e39022.

--- addons/custom/forlife_product_combo/models/product_combo.py
+++ addons/custom/forlife_product_combo/models/product_combo.py
@@ -7,7 +7,7 @@ class ProductCombo(models.Model):
     _name = 'product.combo'
     _description = 'product combo'
 
-    code = fields.Char('Combo code', readonly=True, copy=False, default='New')
+    code = fields.Char('Combo code', readonly=True, required=True, copy=False, default='New')
     description_combo = fields.Text(string="Description combo")
     state = fields.Selection([
         ('new', _('New')),
@@ -24,6 +24,7 @@ class ProductCombo(models.Model):
         ('combo_check_date', 'CHECK(from_date <= to_date)', 'End date may not be before the starting date.')]
 
 
+
     @api.model
     def create(self, vals):
 
@@ -37,10 +38,6 @@ class ProductCombo(models.Model):
                 'combo_id': self.id if self.state in ['in_progress'] else None
             })
 
-            # pr.product_id.product_variant_id.write({
-            #     'combo_id': self.id if self.state in ['in_progress'] else None
-            # })
-
         return result
 
     def write(self, values):
@@ -49,7 +46,7 @@ class ProductCombo(models.Model):
             pr.product_id.write({
                 'combo_id': self.id if self.state in ['in_progress'] else None
             })
-            # pr.product_id.product_variant_id.write({
-            #     'combo_id': self.id if self.state in ['in_progress'] else None
-            # })
+            pr.product_id.product_variant_id.write({
+                'combo_id': self.id if self.state in ['in_progress'] else None
+            })
         return res
