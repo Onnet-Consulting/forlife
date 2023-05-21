9c3c0a6489350baad57a17c4dd998b98b5a3a369
sonw <sonw@example.com>
2023-05-22 00:04:46 +0700

    forlife pos accounting v1

---
+++ addons/custom/forlife_pos_accounting/models/stock_move.py
@@ -0,0 +1,16 @@
+from odoo import models
+
+
+class InheritStockMove(models.Model):
+    _inherit = 'stock.move'
+
+    def _prepare_account_move_vals(
+            self, credit_account_id, debit_account_id, journal_id, qty, description, svl_id, cost
+    ):
+        if self.picking_id.pos_order_id and self.product_id.is_promotion:
+            if self._is_out():
+                credit_account_id = self.product_id.get_product_promotion_expense_account()
+
+        return super(InheritStockMove, self)._prepare_account_move_vals(
+            credit_account_id, debit_account_id, journal_id, qty, description, svl_id, cost
+        )
