8a3a38c27ececbd945f2bc3fdb1741a3ac2469ad
sonw <sonw@example.com>
2023-05-23 09:13:16 +0700

    forlife pos accounting v2

--- addons/custom/forlife_pos_accounting/models/promotion_program.py
+++ addons/custom/forlife_pos_accounting/models/promotion_program.py
@@ -5,4 +5,7 @@ class InheritPromotionProgram(models.Model):
     _inherit = 'promotion.program'
 
     journal_id = fields.Many2one(comodel_name='account.journal', string='Journal', index=True)
-    product_discount_id = fields.Many2one(comodel_name='product.product', string='Discount Product', index=True)
+    product_discount_id = fields.Many2one(
+        comodel_name='product.product', string='Discount Product', index=True, required=True,
+        domain="[('is_promotion', '=', True)]"
+    )
