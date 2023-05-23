dc9a29eee6f10ca60e7ad1ed55457719619b5e89
Tungpd-NES <108250170+Tungpd-NES@users.noreply.github.com>
2023-05-23 21:36:24 +0700

    MFL2201ER-667

--- addons/custom/forlife_permission_management/models/pos_session.py
+++ addons/custom/forlife_permission_management/models/pos_session.py
@@ -6,10 +6,4 @@ from odoo import api, fields, models
 class PosSession(models.Model):
     _inherit = 'pos.session'
 
-    def _get_store_default(self):
-        user_id = self.env['res.users'].browse(self._uid)
-        if not user_id:
-            return
-        return user_id.store_default_id
-
-    store_id = fields.Many2one('store', string='Store', default=_get_store_default)
+    store_id = fields.Many2one('store', string='Store', related='config_id.store_id', related_sudo=True)
