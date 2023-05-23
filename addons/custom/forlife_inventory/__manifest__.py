f00839675217d816e1be2951e88baf0180beded0
ducminh <ducminh>
2023-05-23 13:21:07 +0700

    STEP-1

---
+++ addons/custom/forlife_inventory/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Forlife Inventory",
+    "category": "Forlife Inventory",
+    "version": "1.0.0",
+    "description": """Forlife Inventory""",
+    "depends": [
+        'forlife_stock'
+    ],
+    "data": [
+        'views/stock_location_view.xml'
+    ],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
