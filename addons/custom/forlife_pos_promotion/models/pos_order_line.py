7b84c7a5906498c4cb14a451c814d70a9932741c
Daihv <daihvls96@gmail.com>
2023-05-25 14:25:56 +0700

    [IMP] forlife_pos_promotion: Tạo trường Nguyên giá trên POS order line

--- addons/custom/forlife_pos_promotion/models/pos_order_line.py
+++ addons/custom/forlife_pos_promotion/models/pos_order_line.py
@@ -43,3 +43,4 @@ class PosOrderLine(models.Model):
     _inherit = 'pos.order.line'
 
     promotion_usage_ids = fields.One2many('promotion.usage.line', 'order_line_id')
+    original_price = fields.Float('Original Price', digits=0)
