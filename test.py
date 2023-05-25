7f334a134091d09d24d9462a4bb52de678623609
minhduc <minhduc_onnet>
2023-05-25 13:40:05 +0700

    remove-file-test

--- test.py
+++
@@ -1,4 +0,0 @@
-[{'state': 'draft', 'is_diff_transfer': False, 'employee_id': 1, 'method_transfer':
-    'transfer_between_warehouse', 'location_id': 12681, 'location_dest_id': 16579, 'reference_document': False, 'type': False,
-  'create_date': '2023-05-25 06:14:22', 'stock_request_id': False, 'work_from': False, 'work_to': False, 'total_package'
-: 0, 'total_weight': 0, 'stock_transfer_line': [], 'name': 'PXB853001'}]
