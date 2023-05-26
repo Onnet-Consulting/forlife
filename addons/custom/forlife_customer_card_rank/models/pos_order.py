b4d3242483ec06a66727dac110cf5b66c714e198
Daihv <daihvls96@gmail.com>
2023-05-26 11:41:44 +0700

    [FIX] forlife_customer_card_rank,forlife_pos_point_order: chỉ nhân hệ số với điều kiện là đơn hàng đầu tiên được tích điểm

--- addons/custom/forlife_customer_card_rank/models/pos_order.py
+++ addons/custom/forlife_customer_card_rank/models/pos_order.py
@@ -227,15 +227,15 @@ class PosOrder(models.Model):
             'points_store': points_coefficient
         }
 
-    def get_point_order(self, money_value, brand_id):
-        result = super().get_point_order(money_value, brand_id)
+    def get_point_order(self, money_value, brand_id, is_purchased):
         current_rank_of_customer = (self.partner_id.card_rank_by_brand or {}).get(str(brand_id))
         program = self.program_store_point_id
         if self.allow_for_point and (self.config_id.store_id.id in program.store_ids.ids or not program.store_ids) and current_rank_of_customer and program.card_rank_active:
             accumulate_by_rank = program.accumulate_by_rank_ids.filtered(lambda x: x.card_rank_id.id == current_rank_of_customer[0])
+            coefficient = 1 if is_purchased else (accumulate_by_rank.coefficient or 1)
             if accumulate_by_rank:
-                return int(int((money_value * accumulate_by_rank.accumulative_rate / 100) * (program.card_rank_point_addition / program.card_rank_value_convert)) * (accumulate_by_rank.coefficient or 1))
-        return result
+                return int(int((money_value * accumulate_by_rank.accumulative_rate / 100) * (program.card_rank_point_addition / program.card_rank_value_convert)) * coefficient)
+        return super().get_point_order(money_value, brand_id, is_purchased)
 
 
 class PosOrderLine(models.Model):
