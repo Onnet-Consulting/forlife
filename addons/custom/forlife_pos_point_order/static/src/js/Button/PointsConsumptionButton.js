aaee76350f71b1769fb693910fbf8811d764203a
Minh <minh.nguyen@onnet.vn>
2023-06-01 18:51:32 +0700

    MFL2201ER-806: Khi áp dụng tiêu điểm xóa CTKM mà dòng sản phẩm đó đang áp dụng 2

--- addons/custom/forlife_pos_point_order/static/src/js/Button/PointsConsumptionButton.js
+++ addons/custom/forlife_pos_point_order/static/src/js/Button/PointsConsumptionButton.js
@@ -88,6 +88,10 @@ odoo.define('forlife_pos_point_order.PointsConsumptionButton', function (require
                     }
             };
             var old_data_props = []
+            let currentOrder = this.env.pos.selectedOrder;
+            if (currentOrder._checkHasNotExistedLineOnOldData()) {
+                currentOrder.resetPointOrder();
+            };
             if(!this.env.pos.selectedOrder.old_data){
                 old_data_props = false;
             }else{
