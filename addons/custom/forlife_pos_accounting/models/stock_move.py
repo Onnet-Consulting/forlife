1ee82baa7816897b23fd868017b59b774120471a
sonw <sonw@example.com>
2023-06-03 15:02:36 +0700

    forlife pos accounting: consignment account

--- addons/custom/forlife_pos_accounting/models/stock_move.py
+++ addons/custom/forlife_pos_accounting/models/stock_move.py
@@ -7,9 +7,9 @@ class InheritStockMove(models.Model):
     def _prepare_account_move_vals(
             self, credit_account_id, debit_account_id, journal_id, qty, description, svl_id, cost
     ):
-        if self.picking_id.pos_order_id and self.product_id.is_promotion:
+        if self.picking_id.pos_order_id and self.product_id.check_is_promotion():
             if self._is_out():
-                credit_account_id = self.product_id.get_product_promotion_expense_account()
+                credit_account_id = self.product_id.get_product_gift_account()
 
         return super(InheritStockMove, self)._prepare_account_move_vals(
             credit_account_id, debit_account_id, journal_id, qty, description, svl_id, cost
