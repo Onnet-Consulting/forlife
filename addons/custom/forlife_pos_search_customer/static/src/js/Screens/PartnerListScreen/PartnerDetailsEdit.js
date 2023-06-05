c9d9f46930dd4d68d681d87848a9499cc86fc567
Long Duong <duongthanhlong93@gmail>
2023-06-05 11:19:40 +0700

    [FIX] MFL2201ER-868 the customer information wasn't stored

---
+++ addons/custom/forlife_pos_search_customer/static/src/js/Screens/PartnerListScreen/PartnerDetailsEdit.js
@@ -0,0 +1,21 @@
+odoo.define('forlife_pos_search_customer.CustomPartnerDetailsEdit', function (require) {
+    const PartnerDetailsEdit = require('point_of_sale.PartnerDetailsEdit');
+    const Registries = require('point_of_sale.Registries');
+
+    const CustomPartnerDetailsEdit = PartnerDetailsEdit => class extends PartnerDetailsEdit {
+        setup() {
+            super.setup();
+            let new_fields = ['phone', 'name', 'email'];
+            this.intFields.push.apply(this.intFields, new_fields);
+            new_fields.forEach((field, i) => {
+                if(this.props.partner[field]){
+                    this.changes[field] = this.props.partner[field];
+                }
+            });
+        }
+    }
+
+    Registries.Component.extend(PartnerDetailsEdit, CustomPartnerDetailsEdit);
+
+    return CustomPartnerDetailsEdit;
+})
