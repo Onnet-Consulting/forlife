5ff635444aa5978cb9e69c470c1d345eae8510b8
XMars <xtruongptit@gmail.com>
2023-06-13 10:56:15 +0700

    [IMP] forlife_bravo_integration - update picking data

--- addons/custom/forlife_bravo_integration/models/documents/stock_picking.py
+++ addons/custom/forlife_bravo_integration/models/documents/stock_picking.py
@@ -6,6 +6,7 @@ CONTEXT_PICKING_ACTION = 'bravo_picking_data'
 PICKING_PURCHASE_VALUE = 'picking_purchase'
 PICKING_OTHER_IMPORT_VALUE = 'picking_other_import'
 PICKING_OTHER_EXPORT_VALUE = 'picking_other_export'
+CONTEXT_PICKING_UPDATE = 'bravo_picking_update'
 
 
 class StockPicking(models.Model):
@@ -22,6 +23,7 @@ class StockPicking(models.Model):
     @api.model
     def bravo_get_table(self, **kwargs):
         picking_data = kwargs.get(CONTEXT_PICKING_ACTION)
+        picking_update = kwargs.get(CONTEXT_PICKING_UPDATE)
         bravo_table = 'DEFAULT'
         if picking_data == PICKING_PURCHASE_VALUE:
             bravo_table = "B30AccDocPurchase"
@@ -29,6 +31,8 @@ class StockPicking(models.Model):
             bravo_table = "B30AccDocItemReceipt"
         elif picking_data == PICKING_OTHER_EXPORT_VALUE:
             bravo_table = "B30AccDocItemIssue"
+        elif picking_update:
+            bravo_table = "B30UpdateData"
         return bravo_table
 
     @api.model
@@ -50,12 +54,15 @@ class StockPicking(models.Model):
 
     def bravo_get_insert_values(self, **kwargs):
         journal_data = kwargs.get(CONTEXT_PICKING_ACTION)
+        picking_update = kwargs.get(CONTEXT_PICKING_UPDATE)
         if journal_data == PICKING_PURCHASE_VALUE:
             return self.bravo_get_picking_purchase_values()
         if journal_data == PICKING_OTHER_IMPORT_VALUE:
             return self.bravo_get_picking_other_import_values()
         if journal_data == PICKING_OTHER_EXPORT_VALUE:
             return self.bravo_get_picking_other_export_values()
+        if picking_update:
+            return self.bravo_get_update_picking_values(**kwargs)
         return [], []
 
     def bravo_get_insert_sql_by_picking_action(self):
@@ -85,6 +92,44 @@ class StockPicking(models.Model):
         return queries
 
     def bravo_get_insert_sql(self, **kwargs):
-        if kwargs.get(CONTEXT_PICKING_ACTION):
+        if kwargs.get(CONTEXT_PICKING_ACTION) or kwargs.get(CONTEXT_PICKING_UPDATE):
             return super().bravo_get_insert_sql(**kwargs)
         return self.bravo_get_insert_sql_by_picking_action()
+
+    # -------------------- UPDATE --------------------------
+
+    @api.model
+    def bravo_get_update_picking_columns(self):
+        return [
+            "CompanyCode", "UpdateType", "DocNo", "DocDate",
+            "Stt", "RowId", "ColumnName", "OldValue", "NewValue",
+        ]
+
+    def bravo_get_update_picking_values(self, **kwargs):
+        columns = self.bravo_get_update_picking_columns()
+        values = []
+        for record in self:
+            value = {
+                "CompanyCode": record.company_id.code,
+                "UpdateType": "1",
+                "DocNo": record.name,
+                "DocDate": record.date_done,
+                "Stt": record.id,
+                "RowId": record.id,
+                "ColumnName": "Description",
+                "OldValue": record.note,
+                "NewValue": kwargs.get('note'),
+            }
+            values.append(value)
+        return columns, values
+
+    def write(self, vals):
+        records = self.filtered(lambda r: r.state == 'done')
+        if 'note' not in vals or not bool(records):
+            return super().write(vals)
+        vals.update({CONTEXT_PICKING_UPDATE: True})
+        insert_queries = records.bravo_get_insert_sql(**vals)
+        if insert_queries:
+            self.env[self._name].sudo().with_delay(channel="root.Bravo").bravo_execute_query(insert_queries)
+        vals.pop(CONTEXT_PICKING_UPDATE, None)
+        return super().write(vals)
