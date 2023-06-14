8bebc14a442255d666ae0db50331d35b4ff3f208
taiht <taiht>
2023-06-14 16:23:36 +0700

    sequence account move
    (cherry picked from commit 7555c6558a1b6ff8be87788dcdecba0087187654)

---
+++ addons/custom/forlife_sequence_account_move/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Forlife Sequence Account Move",
+    "category": "account",
+    "version": "1.0.0",
+    "description": """Forlife Sequence Account Move""",
+    "depends": [
+        'account',
+    ],
+    "data": [
+    ],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
