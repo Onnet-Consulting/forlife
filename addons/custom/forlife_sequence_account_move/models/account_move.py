85dfabaaf32396de204c0f6b6440f22c6ba1fd3a
taiht <taiht>
2023-06-15 18:59:47 +0700

    update sequence account move stock valuation
    (cherry picked from commit 85cff53450e7b8a69430936c47b038c775b5d52c)

--- addons/custom/forlife_sequence_account_move/models/account_move.py
+++ addons/custom/forlife_sequence_account_move/models/account_move.py
@@ -7,16 +7,23 @@ class AccountMove(models.Model):
     pos_order_id = fields.Many2one('pos.order', readonly=True, string='POS Order')
 
     def _get_starting_sequence(self):
+        self.ensure_one()
         res = super(AccountMove, self)._get_starting_sequence()
         if self.pos_order_ids:
             warehouse_code = self.pos_order_ids[0].config_id.picking_type_id.warehouse_id.code
             return warehouse_code + '/' + res
+        if self.stock_valuation_layer_ids and self.stock_valuation_layer_ids[0].stock_move_id.picking_id:
+            warehouse_code = self.stock_valuation_layer_ids[0].stock_move_id.picking_id.picking_type_id.warehouse_id.code
+            return warehouse_code + '/' + res
         return res
 
     def _get_last_sequence(self, relaxed=False, with_prefix=None, lock=True):
-        if self.pos_order_ids:
-            self.ensure_one()
-            warehouse_code = self.pos_order_ids[0].config_id.picking_type_id.warehouse_id.code
+        self.ensure_one()
+        if self.pos_order_ids or (self.stock_valuation_layer_ids and self.stock_valuation_layer_ids[0].stock_move_id.picking_id):
+            if self.pos_order_ids:
+                warehouse_code = self.pos_order_ids[0].config_id.picking_type_id.warehouse_id.code
+            if self.stock_valuation_layer_ids and self.stock_valuation_layer_ids[0].stock_move_id.picking_id:
+                warehouse_code = self.stock_valuation_layer_ids[0].stock_move_id.picking_id.picking_type_id.warehouse_id.code
             if self._sequence_field not in self._fields or not self._fields[self._sequence_field].store:
                 raise ValidationError(_('%s is not a stored field', self._sequence_field))
             where_string, param = self._get_last_sequence_domain(relaxed)
