cbfca57656af9ff68745a65214708d0ab8989fd9
NGUYEN VAN HUY <huynv@aht.local>
2023-06-16 14:09:34 +0700

    MFL2201ER-933

--- addons/custom/forlife_purchase_return/models/purchase_order.py
+++ addons/custom/forlife_purchase_return/models/purchase_order.py
@@ -16,8 +16,13 @@ class PurchaseOrder(models.Model):
     return_purchase_ids = fields.One2many('purchase.order', 'origin_purchase_id', string="Return Purchases", copy=False)
     origin_purchase_id = fields.Many2one('purchase.order', string="Origin Purchase", copy=False)
     count_return_purchase = fields.Integer(compute="_compute_count_return_purchase", store=True, copy=False)
-
+    return_picking_count = fields.Integer("Return Shipment count", compute='_compute_return_picking_count')
     warehouse_material = fields.Many2one('stock.location', string="Lý do nhập/xuất khác")
+
+    @api.depends('picking_ids')
+    def _compute_return_picking_count(self):
+        for order in self:
+            order.return_picking_count = len(order.picking_ids)
 
     @api.onchange('partner_id')
     def _onchange_partner_id_return(self):
