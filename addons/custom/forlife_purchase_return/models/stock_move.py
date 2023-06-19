575bdf6301e9638f244537b84758d9ce895c4b61
NGUYEN VAN HUY <huynv@aht.local>
2023-06-19 13:22:52 +0700

    MFL2201ER-945

--- addons/custom/forlife_purchase_return/models/stock_move.py
+++ addons/custom/forlife_purchase_return/models/stock_move.py
@@ -8,8 +8,11 @@ class StockMove(models.Model):
 
     def _prepare_move_line_vals(self, quantity=None, reserved_quant=None):
         vals = super(StockMove, self)._prepare_move_line_vals(quantity, reserved_quant)
-        if self.purchase_line_id and self.purchase_line_id.order_id.is_return:
+        if self.purchase_line_id:
             vals.update({
                 'purchase_uom': self.purchase_line_id.purchase_uom.id,
+                'occasion_code_id': self.purchase_line_id.occasion_code_id.id,
+                'work_production': self.purchase_line_id.production_id.id,
+                'account_analytic_id': self.purchase_line_id.account_analytic_id.id,
             })
         return vals
