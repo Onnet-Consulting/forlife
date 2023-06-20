a46de367956e2a517d399f6c9b79981ed6d7f466
XMars <xtruongptit@gmail.com>
2023-06-20 08:33:09 +0700

    [IMP] forlife_bravo_integration - add cash out

--- addons/custom/forlife_bravo_integration/models/documents/pos_session.py
+++ addons/custom/forlife_bravo_integration/models/documents/pos_session.py
@@ -68,10 +68,42 @@ class PosSessionCashOutMove(models.Model):
 
     @api.model
     def bravo_get_cash_out_move_columns(self):
-        return []
+        return [
+            "CompanyCode", "Stt", "DocCode", "DocNo", "DocDate", "CurrencyCode", "ExchangeRate", "CustomerCode",
+            "CustomerName", "Address", "Description", "EmployeeCode", "BuiltinOrder", "DebitAccount",
+            "CreditAccount", "OriginalAmount", "Amount", "Description1", "DeptCode", "RowId",
+        ]
 
     def bravo_get_cash_out_move_value(self):
         self.ensure_one()
+        credit_lines = self.line_ids.filtered(lambda l: l.credit > 0)
+        credit_line = credit_lines and credit_lines[0]
+        debit_lines = self.line_ids - credit_lines
+        debit_line = debit_lines and debit_lines[0]
+        partner = credit_line.partner_id
+        exchange_rate = 1
         values = []
-
+        journal_value = {
+            "CompanyCode": self.company_id.code,
+            "Stt": self.name,
+            "DocCode": "PT",
+            "DocNo": self.name,
+            "DocDate": self.date,
+            "CurrencyCode": self.currency_id.name,
+            "ExchangeRate": exchange_rate,
+            "CustomerCode": partner.ref,
+            "CustomerName": partner.name,
+            "Address": partner.contact_address_complete,
+            "Description": self.name,
+            "EmployeeCode": self.env.user.employee_id.code,
+            "BuiltinOrder": 1,
+            "DebitAccount": debit_line.account_id.code,
+            "CreditAccount": credit_line.account_id.code,
+            "OriginalAmount": debit_line.debit,
+            "Amount": debit_line.debit,
+            "Description1": debit_line.name,
+            "DeptCode": debit_line.account_analytic_id.code,
+            "RowId": debit_line.id
+        }
+        values.append(journal_value)
         return values
