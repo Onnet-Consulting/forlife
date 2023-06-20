e3f28687e03eb3df6efc4050e40f18ef23f53a3a
Minh <minh.nguyen@onnet.vn>
2023-06-20 16:17:19 +0700

    MFL2201ER-803-update: [47] Hiển thị thêm màu và size trên đơn hàng POS, thêm trường show_orderline_on_pos

--- addons/custom/forlife_point_of_sale/models/product.py
+++ addons/custom/forlife_point_of_sale/models/product.py
@@ -1,6 +1,12 @@
 from odoo import models, fields, _, api
 
 
+class ProductAttribute(models.Model):
+    _inherit = 'product.attribute'
+
+    show_orderline_on_pos = fields.Boolean('Show on POS')
+
+
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
@@ -9,6 +15,7 @@ class ProductProduct(models.Model):
     def _compute_full_attrs_desc(self):
         self.full_attrs_desc = ''
         for product in self:
-            valid_ptals = product.product_tmpl_id.valid_product_template_attribute_line_ids
+            valid_ptals = product.product_tmpl_id.valid_product_template_attribute_line_ids.filtered(
+                lambda att: att.attribute_id.show_orderline_on_pos)
             if valid_ptals:
                 product.full_attrs_desc = '-'.join(valid_ptals.value_ids.mapped('name'))
