ef23ca5a98dd14e46c5fb6758558dfa61e780fa9
minhduc <minhduc_onnet>
2023-06-22 22:07:38 +0700

    import-float_is_zero

--- addons/custom/forlife_inventory/models/stock_move.py
+++ addons/custom/forlife_inventory/models/stock_move.py
@@ -1,5 +1,6 @@
 from odoo import api, fields, models
 from odoo.exceptions import ValidationError
+from odoo.tools import float_is_zero
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
