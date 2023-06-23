599990cdfa26ac2a76c0357be2176e216730b143
Daihv <daihvls96@gmail.com>
2023-06-23 15:01:23 +0700

    [IMP] forlife_report: BÁO CÁO GIÁ TRỊ CHIẾT KHẤU HẠNG THẺ

--- addons/custom/forlife_report/wizard/report_num16.py
+++ addons/custom/forlife_report/wizard/report_num16.py
@@ -8,7 +8,7 @@ from odoo.tools.safe_eval import safe_eval
 TITLES = [
     'STT', 'Ngày', 'Số CT', 'Kho', 'Số CT2', 'Kho2', 'Mã khách', 'Tên khách', 'Đối tượng', 'Nhóm hàng', 'Dòng hàng', 'Kết cấu',
     'Mã vạch', 'Mã hàng', 'Tên hàng', 'Màu sắc', 'Kích cỡ', 'Năm sản xuất', 'Bộ sưu tập', 'Xuất xứ', 'Subclass 1', 'Subclass 2',
-    'Subclass 3', 'Subclass 4', 'Subclass 5', 'Đơn vị tính', 'Nhập', 'Xuất', 'Mã loại', 'Ngày tờ khai', 'Diễn giải',
+    'Subclass 3', 'Subclass 4', 'Subclass 5', 'Đơn vị tính', 'Nhập kho', 'Xuất kho', 'Đơn giá', 'Mã loại', 'Ngày tờ khai', 'Diễn giải',
 ]
 
 
@@ -21,6 +21,7 @@ class ReportNum16(models.TransientModel):
     to_date = fields.Date(string='To date', required=True)
     product_domain = fields.Char('Product', default='[]')
     warehouse_domain = fields.Char('Warehouse', default='[]')
+    move_type = fields.Selection([('all', _('All')), ('in', _('In')), ('out', _('Out'))], 'Move Type', default='all', required=True)
 
     @api.constrains('from_date', 'to_date')
     def check_dates(self):
@@ -34,16 +35,42 @@ class ReportNum16(models.TransientModel):
         tz_offset = self.tz_offset
         attr_value = self.env['res.utility'].get_attribute_code_config()
 
-        where_query = f""" where
-            sm.company_id = any( array{allowed_company})
-            and sm.state = 'done'
-            and {format_date_query("sm.date", tz_offset)} between '{self.from_date}' and '{self.to_date}'
-            and sm.product_id = any (array{product_ids})
-            and (src_wh.id = any (array{warehouse_ids}) or des_wh.id = any (array{warehouse_ids}))
-        """
+        query = []
+        if self.move_type in ('all', 'in'):
+            query.append(f"""
+select sm.id            as move_id,
+       sm.quantity_done as qty_in,
+       0                as qty_out
+from stock_move sm
+         left join stock_location sl1 on sm.location_id = sl1.id
+         left join stock_warehouse wh1 on wh1.id = sl1.warehouse_id
+         left join stock_location sl2 on sm.location_dest_id = sl2.id
+         left join stock_warehouse wh2 on wh2.id = sl2.warehouse_id
+where sm.state = 'done'
+  and sm.company_id = any (array{allowed_company})
+  and {format_date_query("sm.date", tz_offset)} between '{self.from_date}' and '{self.to_date}'
+  and sm.product_id = any (array {product_ids})
+  and wh2.id = any (array {warehouse_ids})
+""")
+        if self.move_type in ('all', 'out'):
+            query.append(f"""
+select sm.id            as move_id,
+       0                as qty_in,
+       sm.quantity_done as qty_out
+from stock_move sm
+         left join stock_location sl1 on sm.location_id = sl1.id
+         left join stock_warehouse wh1 on wh1.id = sl1.warehouse_id
+         left join stock_location sl2 on sm.location_dest_id = sl2.id
+         left join stock_warehouse wh2 on wh2.id = sl2.warehouse_id
+where sm.state = 'done'
+  and sm.company_id = any (array{allowed_company})
+  and {format_date_query("sm.date", tz_offset)} between '{self.from_date}' and '{self.to_date}'
+  and sm.product_id = any (array {product_ids})
+  and wh1.id = any (array {warehouse_ids})
+""")
 
-        query = f"""
-with account_by_categ_id as ( -- lấy mã tài khoản định giá tồn kho bằng cate_id
+        query_final = f"""
+with account_by_categ_id as (
     select 
         cate.id as cate_id,
         aa.code as account_code
@@ -66,83 +93,72 @@ attribute_data as (
     where pp.id = any (array{product_ids}) 
     group by pp.id, pa.attrs_code
 ),
-product_cate_info as (
-    select 
-        pp.id     		                                                          as product_id,
-        cate.complete_name                                                        as complete_name,
-        pp.barcode                                                                as product_barcode,
-        pp.default_code                                                           as internal_ref,
-        coalesce(pt.name::json -> '{user_lang_code}', pt.name::json -> 'en_US')   as product_name,
-        acc.account_code                                                          as account_code,
-        pt.collection                                                             as bo_suu_tap,
-        doi_tuong.value as doi_tuong,
-        mau_sac.value as mau_sac,
-        kich_thuoc.value as kich_thuoc,
-        nam_sx.value as nam_sx,
-        xuat_xu.value as xuat_xu,
-        subclass1.value as subclass1,
-        subclass2.value as subclass2,
-        subclass3.value as subclass3,
-        subclass4.value as subclass4,
-        subclass5.value as subclass5
-    from product_product pp 
-        left join product_template pt on pt.id = pp.product_tmpl_id
-        join product_category cate on cate.id = pt.categ_id
-        left join account_by_categ_id acc on acc.cate_id = cate.id
-        left join attribute_data doi_tuong on doi_tuong.product_id = pp.id and doi_tuong.attrs_code = '{attr_value.get('doi_tuong', '')}'
-        left join attribute_data mau_sac on mau_sac.product_id = pp.id and mau_sac.attrs_code = '{attr_value.get('mau_sac', '')}'
-        left join attribute_data kich_thuoc on kich_thuoc.product_id = pp.id and kich_thuoc.attrs_code = '{attr_value.get('size', '')}'
-        left join attribute_data nam_sx on nam_sx.product_id = pp.id and nam_sx.attrs_code = '{attr_value.get('nam_san_xuat', '')}'
-        left join attribute_data xuat_xu on xuat_xu.product_id = pp.id and xuat_xu.attrs_code = '{attr_value.get('xuat_xu', '')}'
-        left join attribute_data subclass1 on subclass1.product_id = pp.id and subclass1.attrs_code = '{attr_value.get('subclass1', '')}'
-        left join attribute_data subclass2 on subclass2.product_id = pp.id and subclass2.attrs_code = '{attr_value.get('subclass2', '')}'
-        left join attribute_data subclass3 on subclass3.product_id = pp.id and subclass3.attrs_code = '{attr_value.get('subclass3', '')}'
-        left join attribute_data subclass4 on subclass4.product_id = pp.id and subclass4.attrs_code = '{attr_value.get('subclass4', '')}'
-        left join attribute_data subclass5 on subclass5.product_id = pp.id and subclass5.attrs_code = '{attr_value.get('subclass5', '')}'
-    where pp.id = any (array{product_ids})
+stock_moves as (
+    select move_id,
+       sum(qty_in)  as qty_in,
+       sum(qty_out) as qty_out
+       from ({' union all '.join(query)}) as moves
+    group by move_id
 )
-select row_number() over ()                                                         as num,
-       to_char(sm.date, 'DD/MM/YYYY')                                               as date,
-       ''                                                                           as so_ct,
-       ''                                                                           as wh_name,
-       ''                                                                           as so_ct2,
-       ''                                                                           as wh_name2,
-       ''                                                                           as ma_khach,
-       ''                                                                           as ten_khach,
-       pci.doi_tuong                                                                as doi_tuong,
-       split_part(pci.complete_name, ' / ', 2)                                      as nhom_hang,
-       split_part(pci.complete_name, ' / ', 3)                                      as dong_hang,
-       split_part(pci.complete_name, ' / ', 4)                                      as ket_cau,
-       pci.product_barcode                                                          as ma_vach,
-       pci.internal_ref                                                             as ma_hang,
-       pci.product_name                                                             as ten_hang,
-       pci.mau_sac                                                                  as mau_sac,
-       pci.kich_thuoc                                                               as kich_co,
-       pci.nam_sx                                                                   as nam_sx,
-       pci.bo_suu_tap                                                               as bo_suu_tap,
-       pci.xuat_xu                                                                  as xuat_xu,
-       pci.subclass1                                                                as subclass1,
-       pci.subclass2                                                                as subclass2,
-       pci.subclass3                                                                as subclass3,
-       pci.subclass4                                                                as subclass4,
-       pci.subclass5                                                                as subclass5,
+select row_number() over ()                                                         as stt,
+       to_char(sm.date + interval '{tz_offset} h', 'DD/MM/YYYY')                    as ngay,
+       case when sp.transfer_id notnull then sp.origin else sp.name end             as so_ct,
+       coalesce(wh1.name, sl1.name)                                                 as kho_xuat,
+       coalesce(st.name, sp.origin, sm.reference)                                   as so_ct2,
+       coalesce(wh2.name, sl2.name)                                                 as kho_nhap,
+       rp.barcode                                                                   as ma_khach,
+       rp.name                                                                      as ten_khach,
+       doi_tuong.value                                                              as doi_tuong,
+       split_part(cate.complete_name, ' / ', 2)                                     as nhom_hang,
+       split_part(cate.complete_name, ' / ', 3)                                     as dong_hang,
+       split_part(cate.complete_name, ' / ', 4)                                     as ket_cau,
+       pp.barcode                                                                   as ma_vach,
+       pp.sku_code                                                                  as ma_hang,
+       coalesce(pt.name::json -> '{user_lang_code}', pt.name::json -> 'en_US')      as ten_hang,
+       mau_sac.value                                                                as mau_sac,
+       kich_co.value                                                                as kich_co,
+       nam_sx.value                                                                 as nam_sx,
+       pt.collection                                                                as bo_suu_tap,
+       xuat_xu.value                                                                as xuat_xu,
+       subclass1.value                                                              as subclass1,
+       subclass2.value                                                              as subclass2,
+       subclass3.value                                                              as subclass3,
+       subclass4.value                                                              as subclass4,
+       subclass5.value                                                              as subclass5,
        coalesce(uom.name::json -> '{user_lang_code}', uom.name::json -> 'en_US')    as dv_tinh,
-       sm.quantity_done                                                             as nhap,
-       sm.quantity_done                                                             as xuat,
-       pci.account_code                                                             as ma_loai,
+       sms.qty_in                                                                   as nhap,
+       sms.qty_out                                                                  as xuat,
+       pt.list_price                                                                as don_gia,
+       acc.account_code                                                             as ma_loai,
        ''                                                                           as ngay_to_khai,
        sm.name                                                                      as dien_giai
 from stock_move sm
+    join stock_moves sms on sms.move_id = sm.id
+    left join stock_picking sp on sm.picking_id = sp.id
+    left join stock_transfer st on sp.transfer_id = st.id
+    left join res_partner rp on sp.partner_id = rp.id
+    left join stock_location sl1 on sm.location_id = sl1.id
+    left join stock_warehouse wh1 on wh1.id = sl1.warehouse_id
+    left join stock_location sl2 on sm.location_dest_id = sl2.id
+    left join stock_warehouse wh2 on wh2.id = sl2.warehouse_id
+    left join product_product pp on pp.id = sm.product_id
+    left join product_template pt on pt.id = pp.product_tmpl_id
     left join uom_uom uom on uom.id = sm.product_uom
-    left join stock_location des_lc on sm.location_dest_id = des_lc.id
-    left join stock_warehouse des_wh on des_lc.parent_path like concat('%%/', des_wh.view_location_id, '/%%')
-    left join stock_location src_lc on sm.location_id = src_lc.id
-    left join stock_warehouse src_wh on src_lc.parent_path like concat('%%/', src_wh.view_location_id, '/%%')
-    left join product_cate_info pci on pci.product_id = sm.product_id
-{where_query}
-order by num
+    join product_category cate on cate.id = pt.categ_id
+    left join account_by_categ_id acc on acc.cate_id = cate.id
+    left join attribute_data doi_tuong on doi_tuong.product_id = pp.id and doi_tuong.attrs_code = '{attr_value.get('doi_tuong', '')}'
+    left join attribute_data mau_sac on mau_sac.product_id = pp.id and mau_sac.attrs_code = '{attr_value.get('mau_sac', '')}'
+    left join attribute_data kich_co on kich_co.product_id = pp.id and kich_co.attrs_code = '{attr_value.get('size', '')}'
+    left join attribute_data nam_sx on nam_sx.product_id = pp.id and nam_sx.attrs_code = '{attr_value.get('nam_san_xuat', '')}'
+    left join attribute_data xuat_xu on xuat_xu.product_id = pp.id and xuat_xu.attrs_code = '{attr_value.get('xuat_xu', '')}'
+    left join attribute_data subclass1 on subclass1.product_id = pp.id and subclass1.attrs_code = '{attr_value.get('subclass1', '')}'
+    left join attribute_data subclass2 on subclass2.product_id = pp.id and subclass2.attrs_code = '{attr_value.get('subclass2', '')}'
+    left join attribute_data subclass3 on subclass3.product_id = pp.id and subclass3.attrs_code = '{attr_value.get('subclass3', '')}'
+    left join attribute_data subclass4 on subclass4.product_id = pp.id and subclass4.attrs_code = '{attr_value.get('subclass4', '')}'
+    left join attribute_data subclass5 on subclass5.product_id = pp.id and subclass5.attrs_code = '{attr_value.get('subclass5', '')}'
+order by stt
 """
-        return query
+        return query_final
 
     def get_data(self, allowed_company):
         allowed_company = allowed_company or [-1]
@@ -171,12 +187,12 @@ order by num
         sheet.set_column(0, len(TITLES), 20)
         row = 5
         for value in data['data']:
-            sheet.write(row, 0, value.get('num'), formats.get('center_format'))
-            sheet.write(row, 1, value.get('date'), formats.get('center_format'))
+            sheet.write(row, 0, value.get('stt'), formats.get('center_format'))
+            sheet.write(row, 1, value.get('ngay'), formats.get('center_format'))
             sheet.write(row, 2, value.get('so_ct'), formats.get('normal_format'))
-            sheet.write(row, 3, value.get('wh_name'), formats.get('normal_format'))
+            sheet.write(row, 3, value.get('kho_xuat'), formats.get('normal_format'))
             sheet.write(row, 4, value.get('so_ct2'), formats.get('normal_format'))
-            sheet.write(row, 5, value.get('wh_name2'), formats.get('normal_format'))
+            sheet.write(row, 5, value.get('kho_nhap'), formats.get('normal_format'))
             sheet.write(row, 6, value.get('ma_khach'), formats.get('normal_format'))
             sheet.write(row, 7, value.get('ten_khach'), formats.get('normal_format'))
             sheet.write(row, 8, ', '.join(value.get('doi_tuong') or []), formats.get('normal_format'))
@@ -199,7 +215,8 @@ order by num
             sheet.write(row, 25, value.get('dv_tinh'), formats.get('normal_format'))
             sheet.write(row, 26, value.get('nhap'), formats.get('normal_format'))
             sheet.write(row, 27, value.get('xuat'), formats.get('normal_format'))
-            sheet.write(row, 28, value.get('ma_loai'), formats.get('normal_format'))
-            sheet.write(row, 29, value.get('ngay_to_khai'), formats.get('normal_format'))
-            sheet.write(row, 30, value.get('dien_giai'), formats.get('normal_format'))
+            sheet.write(row, 28, value.get('don_gia'), formats.get('int_number_format'))
+            sheet.write(row, 29, value.get('ma_loai'), formats.get('normal_format'))
+            sheet.write(row, 30, value.get('ngay_to_khai'), formats.get('normal_format'))
+            sheet.write(row, 31, value.get('dien_giai'), formats.get('normal_format'))
             row += 1
