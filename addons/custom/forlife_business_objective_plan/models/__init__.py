81ec8a2b204c574cb4bc5eac7691a04f786ebded
Daihv <daihvls96@gmail.com>
2023-06-29 09:39:06 +0700

    [IMP] forlife_business_objective_plan: update

--- addons/custom/forlife_business_objective_plan/models/__init__.py
+++ addons/custom/forlife_business_objective_plan/models/__init__.py
@@ -4,3 +4,4 @@ from . import business_objective_plan
 from . import business_objective_store
 from . import business_objective_employee
 from . import employee_transfer
+from . import search_by_name_code
