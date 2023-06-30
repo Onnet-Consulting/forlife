9e150a6bd13d78b18f4baa15547e513a2b513de2
Nguyễn Sỹ Tây (Onnet (Part time)) <sytay.20@gmail.com>
2023-06-30 07:09:27 +0000

    Taysy sale promotion UAt

--- addons/custom/forlife_sale_promotion/models/sale_promotion.py
+++ addons/custom/forlife_sale_promotion/models/sale_promotion.py
@@ -13,4 +13,13 @@ class SaleOrderPromotion(models.Model):
     account_id = fields.Many2one('account.account', string="Account")
     description = fields.Char(string="Description")
     order_id = fields.Many2one("sale.order", string="Order")
+    promotion_type = fields.Selection([
+        ('diff_price', 'diff_price'),
+        ('discount', 'discount'),
+        ('vip_amount_remain', 'vip_amount_remain'),
+        ('vip_amount', 'vip_amount'),
+        ('nhanh_shipping_fee', 'nhanh_shipping_fee'),
+        ('customer_shipping_fee', 'customer_shipping_fee'),
+        ('reward', 'reward')
+    ], string='promotion_type')
     analytic_account_id = fields.Many2one('account.analytic.account', string="Analytic account")
