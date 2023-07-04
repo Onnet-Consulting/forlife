e65a27e2df5649b6414a4bf33cc7fa41d1a5c9d7
BEER <tienbd5@viettel.com.vn>
2023-07-04 23:36:00 +0700

    Fix field location in Sale_oder + fix bug API bravo return list None

--- addons/custom/forlife_sale/models/sale_order.py
+++ addons/custom/forlife_sale/models/sale_order.py
@@ -412,7 +412,7 @@ class SaleOrderLine(models.Model):
 
     @api.onchange('product_id')
     def _onchange_product_get_domain(self):
-        location = self.order_id.warehouse_id.lot_stock_id if self.order_id.warehouse_id else None
+        # location = self.order_id.warehouse_id.lot_stock_id if self.order_id.warehouse_id else None
         self.x_account_analytic_id = self.order_id.x_account_analytic_ids[0]._origin if self.order_id.x_account_analytic_ids else None
         self.x_occasion_code_id = self.order_id.x_occasion_code_ids[0]._origin if self.order_id.x_occasion_code_ids else None
         self.x_manufacture_order_code_id = self.order_id.x_manufacture_order_code_id
