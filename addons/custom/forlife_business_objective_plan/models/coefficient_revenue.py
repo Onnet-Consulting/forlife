3e291afafc30487d9f4965d10d4e1d95f258a06a
Daihv <daihvls96@gmail.com>
2023-07-07 14:55:09 +0700

    [FIX] forlife_business_objective_plan: fix field fixed_coefficient

--- addons/custom/forlife_business_objective_plan/models/coefficient_revenue.py
+++ addons/custom/forlife_business_objective_plan/models/coefficient_revenue.py
@@ -10,7 +10,8 @@ class CoefficientRevenue(models.Model):
 
     name = fields.Char('Name', required=True)
     job_id = fields.Many2one('hr.job', 'Job Position', ondelete='restrict', copy=False)
-    fixed_coefficient = fields.Float('Fixed Coefficient', default=0, copy=True)
+    fixed_coefficient_direct = fields.Float('Fixed Coefficient direct', default=0, copy=True)
+    fixed_coefficient_indirect = fields.Float('Fixed Coefficient indirect', default=0, copy=True)
     pc_by_employee_ids = fields.One2many('percentage.complete.by.employee', inverse_name='coefficient_revenue_id', string='Direct revenue', copy=True)
     pc_by_store_ids = fields.One2many('percentage.complete.by.store', inverse_name='coefficient_revenue_id', string='Indirect revenue', copy=True)
     brand_id = fields.Many2one("res.brand", string="Brand", required=True, copy=True)
