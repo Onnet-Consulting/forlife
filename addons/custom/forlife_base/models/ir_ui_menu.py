fd65c88cc5585fc346216f18d2424dc5b8797657
Tungpd-NES <108250170+Tungpd-NES@users.noreply.github.com>
2023-07-08 00:14:57 +0700

    resolve MFL2201ER-891

--- addons/custom/forlife_base/models/ir_ui_menu.py
+++ addons/custom/forlife_base/models/ir_ui_menu.py
@@ -7,12 +7,13 @@ from odoo import fields, models, api, _
 class IrUIMenu(models.Model):
     _inherit = 'ir.ui.menu'
 
-    model_id = fields.Many2one('ir.model', string='Model', compute='_get_model_id')
+    model_id = fields.Many2one('ir.model', string='Model', compute='_get_model_id', store=True, compute_sudo=True)
 
     @api.depends('action')
     def _get_model_id(self):
-        for menu in self.filtered(lambda m: m.action):
-            if menu.action._name != 'ir.actions.act_window':
+        for menu in self:
+            if not menu.action or menu.action._name != 'ir.actions.act_window':
+                menu.model_id = False
                 continue
             model_name = menu.action.res_model
             menu.model_id = menu.model_id.search([('model', '=', model_name)], limit=1)
