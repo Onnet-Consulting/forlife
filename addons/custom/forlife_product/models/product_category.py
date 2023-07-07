ad426cee55bcb656e1d3fd27b675fdea214116b3
ducminh <ducminh>
2023-07-07 09:54:30 +0700

    master-data-for-product

--- addons/custom/forlife_product/models/product_category.py
+++ addons/custom/forlife_product/models/product_category.py
@@ -4,3 +4,6 @@ class ProductCategory(models.Model):
     _inherit = 'product.category'
 
     asset_group = fields.Boolean('Nhóm tài sản')
+    structure_code = fields.Char('Mã kết cấu')
+    old_name = fields.Char('Tên cũ')
+    career_management = fields.Selection([('fashion','Fashion'), ('gdpk','GDPK')], string='Ngành hàng quản trị')
