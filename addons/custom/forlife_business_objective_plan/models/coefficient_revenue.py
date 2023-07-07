ebd1a3350cb96376114d7abf833d92c8d395d60c
Daihv <daihvls96@gmail.com>
2023-07-07 11:38:59 +0700

    [FIX] forlife_business_objective_plan: fix duplicate coefficient revenue

--- addons/custom/forlife_business_objective_plan/models/coefficient_revenue.py
+++ addons/custom/forlife_business_objective_plan/models/coefficient_revenue.py
@@ -9,11 +9,11 @@ class CoefficientRevenue(models.Model):
     _description = 'Coefficient Revenue'
 
     name = fields.Char('Name', required=True)
-    job_id = fields.Many2one('hr.job', 'Job Position', ondelete='restrict', required=True)
-    fixed_coefficient = fields.Float('Fixed Coefficient', default=0)
-    pc_by_employee_ids = fields.One2many('percentage.complete.by.employee', inverse_name='coefficient_revenue_id', string='Direct revenue')
-    pc_by_store_ids = fields.One2many('percentage.complete.by.store', inverse_name='coefficient_revenue_id', string='Indirect revenue')
-    brand_id = fields.Many2one("res.brand", string="Brand", required=True)
+    job_id = fields.Many2one('hr.job', 'Job Position', ondelete='restrict', copy=False)
+    fixed_coefficient = fields.Float('Fixed Coefficient', default=0, copy=True)
+    pc_by_employee_ids = fields.One2many('percentage.complete.by.employee', inverse_name='coefficient_revenue_id', string='Direct revenue', copy=True)
+    pc_by_store_ids = fields.One2many('percentage.complete.by.store', inverse_name='coefficient_revenue_id', string='Indirect revenue', copy=True)
+    brand_id = fields.Many2one("res.brand", string="Brand", required=True, copy=True)
 
     _sql_constraints = [
         ('unique_job_position', 'UNIQUE(job_id, brand_id)', 'Job Position must be unique per brand !')
