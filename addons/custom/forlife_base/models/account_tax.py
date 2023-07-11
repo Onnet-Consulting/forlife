1559e080649044590fb8387842ebdf7a980e819f
Nguyen Van Huy <huy.nguyen@arrowhitech.com>
2023-07-11 18:44:35 +0700

    _compute_taxes_for_single_line

--- addons/custom/forlife_base/models/account_tax.py
+++ addons/custom/forlife_base/models/account_tax.py
@@ -10,23 +10,23 @@ class AccountTax(models.Model):
         ('unique_code', 'UNIQUE(code, company_id)', 'Code must be unique per company!')
     ]
 
-    @api.model
-    def _compute_taxes_for_single_line(
-        self, 
-        base_line, 
-        handle_price_include=True, 
-        include_caba_tags=False, 
-        early_pay_discount_computation=None, 
-        early_pay_discount_percentage=None
-    ):
-        if float(base_line["record"].x_cart_discount_fixed_price) > 0:
-            base_line['discount'] = (base_line["record"].x_cart_discount_fixed_price/base_line['price_unit']) * 100
-
-        to_update_vals, tax_values_list = super()._compute_taxes_for_single_line(
-            base_line,
-            handle_price_include=handle_price_include,
-            include_caba_tags=include_caba_tags,
-            early_pay_discount_computation=early_pay_discount_computation,
-            early_pay_discount_percentage=early_pay_discount_percentage
-        )
-        return to_update_vals, tax_values_list
+    # @api.model
+    # def _compute_taxes_for_single_line(
+    #     self,
+    #     base_line,
+    #     handle_price_include=True,
+    #     include_caba_tags=False,
+    #     early_pay_discount_computation=None,
+    #     early_pay_discount_percentage=None
+    # ):
+    #     if float(base_line["record"].x_cart_discount_fixed_price) > 0:
+    #         base_line['discount'] = (base_line["record"].x_cart_discount_fixed_price/base_line['price_unit']) * 100
+    #
+    #     to_update_vals, tax_values_list = super()._compute_taxes_for_single_line(
+    #         base_line,
+    #         handle_price_include=handle_price_include,
+    #         include_caba_tags=include_caba_tags,
+    #         early_pay_discount_computation=early_pay_discount_computation,
+    #         early_pay_discount_percentage=early_pay_discount_percentage
+    #     )
+    #     return to_update_vals, tax_values_list
