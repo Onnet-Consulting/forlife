870174c2afc8830055777cbf34b58bcc97b4f2ce
kenny <thangtv@arrowhitech.com>
2023-07-12 08:48:32 +0700

    add try catch

--- addons/custom/forlife_base/models/account_tax.py
+++ addons/custom/forlife_base/models/account_tax.py
@@ -19,8 +19,12 @@ class AccountTax(models.Model):
         early_pay_discount_computation=None, 
         early_pay_discount_percentage=None
     ):
-        if float(base_line["record"].x_cart_discount_fixed_price) > 0:
-            base_line['discount'] = (base_line["record"].x_cart_discount_fixed_price/base_line['price_unit']) * 100
+        try:
+            if float(base_line["record"].x_cart_discount_fixed_price) > 0:
+                base_line['discount'] = (base_line["record"].x_cart_discount_fixed_price/base_line['price_unit']) * 100
+        except Exception as e:
+            pass
+        
 
         to_update_vals, tax_values_list = super()._compute_taxes_for_single_line(
             base_line,
