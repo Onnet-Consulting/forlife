7fd436f15b5bd5a8d0daa2e666678a6646e9872b
taiht <taiht>
2023-07-17 13:05:58 +0700

    fix conflict

--- addons/custom/forlife_base/models/product_product.py
+++ addons/custom/forlife_base/models/product_product.py
@@ -36,6 +36,7 @@ class ProducProduct(models.Model):
     alert_date = fields.Datetime(string='Alert Date', compute='_compute_dates', store=True, readonly=False)
     product_expiry_reminded = fields.Boolean(string="Expiry has been reminded", default=False)
     brand_id = fields.Many2one('res.brand', related='product_tmpl_id.brand_id', string='Brand', readonly=1)
+    is_need_scan_barcode = fields.Boolean(default=False, string='Is Need Scan Barcode')
 
     @api.depends('expiration_date', 'days_before_alert')
     def _compute_dates(self):
