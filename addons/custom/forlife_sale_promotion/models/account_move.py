6bdb333cdc20e018a5d009c240cf52e8478b195b
Nguyễn Sỹ Tây (Onnet (Part time)) <sytay.20@gmail.com>
2023-07-17 23:23:05 +0700

    fix promotion tax

--- addons/custom/forlife_sale_promotion/models/account_move.py
+++ addons/custom/forlife_sale_promotion/models/account_move.py
@@ -72,9 +72,6 @@ class AccountMove(models.Model):
                 if not account_payable_customer_id:
                     raise UserError("Chưa cấu hình tài khoản phải trả cho khách hàng")
 
-                account_tax = pr.product_id.taxes_id
-                account_repartition_tax = account_tax and account_tax[0].invoice_repartition_line_ids.filtered(lambda p: p.repartition_type == 'tax')
-
                 if pr.promotion_type in ['vip_amount', 'reward']:
                     line_allow = True
                     account_debit_id = pr.value > 0 and pr.account_id or property_account_receivable_id
@@ -90,12 +87,24 @@ class AccountMove(models.Model):
                     account_credit_id = account_payable_customer_id
 
                 if line_allow:
+                    account_tax = pr.product_id.taxes_id
+                    account_tax_id = False
+                    product_with_tax_value = abs(pr.value)
+                    product_value_without_tax = abs(pr.value)
+                    product_tax_value = 0
+                    if account_tax and len(account_tax):
+                        account_tax_ids = account_tax[0].invoice_repartition_line_ids.filtered(lambda p: p.repartition_type == 'tax')
+                        if len(account_tax_ids) and account_tax_ids[0].account_id:
+                            account_tax_id = account_tax_ids[0].account_id
+                            product_value_without_tax = account_tax_id and product_with_tax_value - (product_with_tax_value * account_tax[0].amount / 100)
+                            product_tax_value = product_with_tax_value - product_value_without_tax
+
                     line_ids.append({
                         'name': self.name + "(%s)" % pr.description,
                         'product_id': pr.product_id.id,
                         'account_id': account_debit_id and account_debit_id.id,
                         'analytic_account_id': pr.analytic_account_id.id,
-                        'debit': abs(pr.value),
+                        'debit': product_value_without_tax,
                         'credit': 0
                     })
                     line_ids.append({
@@ -104,26 +113,24 @@ class AccountMove(models.Model):
                         'account_id': account_credit_id and account_credit_id.id,
                         'analytic_account_id': pr.analytic_account_id.id,
                         'debit': 0,
-                        'credit': abs(pr.value)
+                        'credit': product_value_without_tax
                     })
-                    if pr.promotion_type == 'nhanh_shipping_fee':
-                        if not account_repartition_tax or not account_repartition_tax[0].account_id:
-                            raise UserError("Chưa cấu hình tài khoản thuế cho sản phầm!")
 
+                    if account_tax_id:
                         line_ids.append({
                             'name': self.name + "(%s)" % pr.description,
                             'product_id': pr.product_id.id,
-                            'account_id': account_repartition_tax and account_repartition_tax[0].account_id.id,
+                            'account_id': account_tax_id and account_tax_id.id,
                             'analytic_account_id': pr.analytic_account_id.id,
                             'debit': 0,
-                            'credit': abs(pr.value)
+                            'credit': product_tax_value
                         })
                         line_ids.append({
                             'name': self.name + "(%s)" % pr.description,
                             'product_id': pr.product_id.id,
-                            'account_id': account_repartition_tax and account_repartition_tax[0].account_id.id,
+                            'account_id': account_tax_id and account_tax_id.id,
                             'analytic_account_id': pr.analytic_account_id.id,
-                            'debit': abs(pr.value),
+                            'debit': product_tax_value,
                             'credit': 0
                         })
 
