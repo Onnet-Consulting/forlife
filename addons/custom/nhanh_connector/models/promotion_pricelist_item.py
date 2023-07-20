f5076bb4a6206e4dd1cc16a7fa37c7f1717da67a
Hoàng Trọng Tài (Onnet) <tai.hoang@onnet.vn>
2023-07-20 14:56:17 +0000

    Update promotion_pricelist_item.py

--- addons/custom/nhanh_connector/models/promotion_pricelist_item.py
+++ addons/custom/nhanh_connector/models/promotion_pricelist_item.py
@@ -6,16 +6,16 @@ from odoo import models, fields, api, _, tools
 class PromotionPricelistItem(models.Model):
     _inherit = 'promotion.pricelist.item'
 
-    is_for_nhanh = fields.Boolean(related='program_id.is_for_nhanh', string='CTKM cho nhanh', store=True)
-    state = fields.Selection(related='program_id.state', string='Campaign State', store=True)
-    to_date = fields.Datetime(related='program_id.to_date', string='To Date', store=True)
-    from_date = fields.Datetime(related='program_id.from_date', string='From Date',  store=True)
+    is_for_nhanh = fields.Boolean(related='program_id.is_for_nhanh', string='CTKM cho nhanh', store=False)
+    state = fields.Selection(related='program_id.state', string='Campaign State', store=False)
+    to_date = fields.Datetime(related='program_id.to_date', string='To Date', store=False)
+    from_date = fields.Datetime(related='program_id.from_date', string='From Date',  store=False)
 
-    campaign_id = fields.Many2one('promotion.campaign', name='Campaign', related='program_id.campaign_id', store=True)
+    campaign_id = fields.Many2one('promotion.campaign', name='Campaign', related='program_id.campaign_id', store=False)
 
     barcode = fields.Char(related='product_id.barcode')
     product_tmpl_id = fields.Many2one('product.template', 
         string='Product Template',
         related='product_id.product_tmpl_id',
-        store=True
+        store=False
     )
