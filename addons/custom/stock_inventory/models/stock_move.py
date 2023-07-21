4066f4ebd12cb3768a0c1e7b7a0753573da96253
Nguyen Van Huy <huy.nguyen@arrowhitech.com>
2023-07-21 11:24:38 +0700

    Fix date account move

--- addons/custom/stock_inventory/models/stock_move.py
+++ addons/custom/stock_inventory/models/stock_move.py
@@ -1,6 +1,5 @@
 from odoo import models, _
-from odoo.exceptions import ValidationError
-
+from datetime import datetime, timedelta
 
 class InheritStockMove(models.Model):
     _inherit = 'stock.move'
@@ -8,8 +7,9 @@ class InheritStockMove(models.Model):
     def _prepare_account_move_vals(self, credit_account_id, debit_account_id, journal_id, qty, description, svl_id, cost):
         res = super(InheritStockMove, self)._prepare_account_move_vals(credit_account_id, debit_account_id, journal_id, qty, description, svl_id, cost)
         if self.inventory_id:
+            invoice_date = (self.inventory_id.date + timedelta(hours=7)).date()
             res.update({
-                'date': self.inventory_id.date.date(),
-                'invoice_date_due': self.inventory_id.date.date(),
+                'date': invoice_date,
+                'invoice_date_due': invoice_date,
             })
         return res
