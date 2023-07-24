54242f9d6077c34c9998dd4600b2b82c384557b9
Nguyễn Văn Huy (Onnet) <huy.nguyen@onnet.vn>
2023-07-24 02:25:09 +0000

    Update account_move.py

--- addons/custom/forlife_sale_promotion/models/account_move.py
+++ addons/custom/forlife_sale_promotion/models/account_move.py
@@ -50,6 +50,7 @@ class AccountMove(models.Model):
                 account_payable_customer_id = self.partner_id.property_account_payable_id
                 if not account_payable_customer_id:
                     raise UserError("Chưa cấu hình tài khoản phải trả cho khách hàng")
+                    
                 account_tax = pr.product_id.taxes_id.filtered(lambda x: x.company_id.id == self.env.company.id)
                 account_repartition_tax = account_tax and account_tax[0].invoice_repartition_line_ids.filtered(lambda p: p.repartition_type == 'tax')
                 if pr.promotion_type in ['vip_amount', 'reward']:
