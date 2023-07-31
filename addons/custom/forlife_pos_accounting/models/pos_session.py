77e4f0865a6c03eda97c12aeeb4dba9d46ab7aeb
Minh <minh.nguyen@onnet.vn>
2023-07-31 16:18:36 +0700

    MFL2201ER-1137: features TC [Hạch toán POS] Hạch toán đối tượng Cửa hàng cho bank, subtask 10, 11, 12

--- addons/custom/forlife_pos_accounting/models/pos_session.py
+++ addons/custom/forlife_pos_accounting/models/pos_session.py
@@ -13,10 +13,18 @@ class PosSession(models.Model):
             data['split_cash_statement_lines'].filtered(lambda line: not line.partner_id).partner_id = store_partner
         return new_data
 
-    # def _create_bank_payment_moves(self, data):
-    #     new_data = super(PosSession, self)._create_bank_payment_moves(data)
-    #     store_partner = self.config_id.store_id.contact_id
-    #     if data['payment_method_to_receivable_lines'] and store_partner:
-    #         for payment, receivable_lines in data['payment_method_to_receivable_lines'].items():
-    #             receivable_lines.filtered(lambda line: not line.partner_id).partner_id = store_partner
-    #     return new_data
+    # Thiết lập đối tượng cửa hàng vào chi tiết bút toán PTTT có partner_id = False
+    # ======================== #
+    # 1.Trường hợp kết hợp các giao dịch thanh toán ngân hàng
+    def _create_combine_account_payment(self, payment_method, amounts, diff_amount):
+        """ """
+        result = super()._create_combine_account_payment(payment_method, amounts, diff_amount)
+        store_partner = self.config_id.store_id.contact_id
+        payment_method_receivable_line = result.filtered(
+            lambda l: l.payment_id.pos_payment_method_id.id == payment_method.id and not l.partner_id)
+        if store_partner and payment_method_receivable_line:
+            payment_method_receivable_line.payment_id.move_id.line_ids.partner_id = store_partner
+        return result
+
+    # 2. Trường hợp tách các giao dịch thanh toán ngân hàng
+    #  Case này partner_id luôn được thiết lập đối tượng là khách hàng tại method _create_split_account_payment
