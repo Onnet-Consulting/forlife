822a6d311cb27677f6871a80fc2ba3198b1cc2f7
Daihv <daihvls96@gmail.com>
2023-08-05 18:38:28 +0700

    [FIX] forlife: tính năng bật tắt đồng bộ Bravo, RabbitMQ. Bổ sung đồng bộ tay

--- addons/custom/forlife_rabbitmq_integration/models/partner_card_rank_line.py
+++ addons/custom/forlife_rabbitmq_integration/models/partner_card_rank_line.py
@@ -8,8 +8,9 @@ class PartnerCardRankLine(models.Model):
     _inherit = ['partner.card.rank.line', 'sync.info.rabbitmq.create']
     _create_action = 'update'
 
+    @api.model
     def domain_record_sync_info(self):
-        return self.filtered(lambda f: f.old_card_rank_id != f.new_card_rank_id)
+        return [('is_change_rank', '=', True)]
 
     def get_sync_info_value(self):
         rec = self.mapped('partner_card_rank_id.customer_id')
