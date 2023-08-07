7cb04d85201d3de175e205852df772d776af128d
Daihv <daihvls96@gmail.com>
2023-08-08 00:55:24 +0700

    [IMP] forlife_bravo_integration: update code

--- addons/custom/forlife_bravo_integration/models/documents/synthetic_daily_transfer.py
+++ addons/custom/forlife_bravo_integration/models/documents/synthetic_daily_transfer.py
@@ -2,6 +2,8 @@
 
 from odoo import api, fields, models, _
 from datetime import datetime, timedelta
+from itertools import groupby
+from operator import itemgetter
 
 
 class SyntheticDailyTransfer(models.Model):
@@ -41,13 +43,87 @@ class SyntheticDailyTransfer(models.Model):
         source_is_deposit = pickings.filtered(lambda f: f.location_id.id_deposit and not f.location_dest_id.id_deposit)
         dest_is_deposit = pickings.filtered(lambda f: not f.location_id.id_deposit and f.location_dest_id.id_deposit)
         other_picking = pickings - source_is_deposit - dest_is_deposit
-        for picking in source_is_deposit:
-            pass
-        self.sudo().create({
-            'date': date,
-            'company_id': company.id,
-        })
+        line_ids = self.prepare_data('source', 'Thu hồi hàng ký gửi', source_is_deposit, company, date) if source_is_deposit else []
+        line_ids.extend(self.prepare_data('dest', 'Xuất hàng ký gửi', dest_is_deposit, company, date) if dest_is_deposit else [])
+        line_ids.extend(self.prepare_data('other', 'Xuất điều chuyển nội bộ', other_picking, company, date) if other_picking else [])
+        if line_ids:
+            self.sudo().create({
+                'date': date,
+                'company_id': company.id,
+                'line_ids': line_ids,
+            })
 
+    @api.model
+    def prepare_data(self, type, description, pickings, company, date):
+        if not pickings:
+            return []
+        data = []
+        for picking in pickings:
+            val = {
+                'source_warehouse': picking.location_id.warehouse_id.code or '',
+                'destination_warehouse': picking.location_dest_id.warehouse_id.code or '',
+            }
+            for stock_move in picking.move_ids:
+                val.update({
+                    'product_code': stock_move.product_id.barcode or '',
+                    'product_name': stock_move.product_id.name or '',
+                    'product_uom': stock_move.product_id.uom_id.code or '',
+                    'debit_account': stock_move.product_id.categ_id.property_stock_valuation_account_id.code or '',
+                    'credit_account': stock_move.product_id.categ_id.property_stock_valuation_account_id.code or '',
+                    'qty': stock_move.quantity_done,
+                    'price': 0,
+                    'amount_total': 0,
+                    'occasion_code': stock_move.occasion_code_id.code or '',
+                    'account_analytic': stock_move.account_analytic_id.code or '',
+                    'work_production': stock_move.work_production.code or '',
+                })
+                data.append(val)
+        key_getter1 = itemgetter('source_warehouse', 'destination_warehouse')
+        value_getter1 = itemgetter('product_code', 'product_name', 'product_uom', 'debit_account', 'credit_account', 'qty', 'price', 'amount_total', 'occasion_code', 'account_analytic', 'work_production')
+        key_getter2 = itemgetter('product_code', 'product_name', 'product_uom', 'debit_account', 'credit_account', 'occasion_code', 'account_analytic', 'work_production', 'price', 'amount_total')
+        value_getter2 = itemgetter('qty', 'qty')
+        result1 = []
+        for (source_wh, destination_wh), objs1 in groupby(sorted(data, key=key_getter1), key_getter1):
+            vals1 = []
+            v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11 = zip(*map(value_getter1, objs1))
+            for idx, v in enumerate(v1):
+                vals1.append({
+                    'product_code': v1[idx],
+                    'product_name': v2[idx],
+                    'product_uom': v3[idx],
+                    'debit_account': v4[idx],
+                    'credit_account': v5[idx],
+                    'qty': v6[idx],
+                    'price': v7[idx],
+                    'amount_total': v8[idx],
+                    'occasion_code': v9[idx],
+                    'account_analytic': v10[idx],
+                    'work_production': v11[idx],
+                })
+            result2 = []
+            for key, objs2 in groupby(sorted(vals1, key=key_getter2), key_getter2):
+                qty, x = zip(*map(value_getter2, objs2))
+                result2.append((0, 0, {
+                    'product_code': key[0],
+                    'product_name': key[1],
+                    'product_uom': key[2],
+                    'debit_account': key[3],
+                    'credit_account': key[4],
+                    'occasion_code': key[5],
+                    'account_analytic': key[6],
+                    'work_production': key[7],
+                    'qty': sum(qty) or 0,
+                    'price': key[8],
+                    'amount_total': key[9],
+                }))
+            result1.append((0, 0, {
+                'source_warehouse': source_wh,
+                'destination_warehouse': destination_wh,
+                'company_code': company.code,
+                'description': description,
+                'detail_ids': result2,
+            }))
+        return result1
 
 
 class SyntheticDailyTransferLocation(models.Model):
@@ -56,8 +132,6 @@ class SyntheticDailyTransferLocation(models.Model):
     _order = 'synthetic_id desc, id desc'
 
     synthetic_id = fields.Many2one('synthetic.daily.transfer', 'Phiếu tổng hợp', ondelete='restrict')
-    location_id = fields.Many2one('stock.location', 'Địa điểm nguồn')
-    location_dest_id = fields.Many2one('stock.location', 'Địa điểm đích')
     company_code = fields.Char('Công ty')
     transfer_code = fields.Char('Mã phiếu')
     description = fields.Char('Diễn giải')
@@ -97,7 +171,6 @@ class SyntheticDailyTransferLine(models.Model):
     _order = 'sdt_location_id desc, id desc'
 
     sdt_location_id = fields.Many2one('synthetic.daily.transfer.location', 'Điều chuyển', ondelete='restrict')
-    product_id = fields.Many2one('product.product', 'Sản phẩm')
     product_code = fields.Char('Mã sản phẩm')
     product_name = fields.Char('Tên sản phẩm')
     product_uom = fields.Char('Đơn vị tính')
