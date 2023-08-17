d142ed301167bf17d1329fa4f5709a33099ee3ae
Nguyen Van Huy <huy.nguyen@arrowhitech.com>
2023-08-17 21:57:10 +0700

    Clear code forlife_purchase

--- addons/custom/forlife_purchase/models/__init__.py
+++ addons/custom/forlife_purchase/models/__init__.py
@@ -1,16 +1,26 @@
+from . import account_move_line
+from . import barcode
+from . import forlife_bom
+from . import forlife_event
+from . import forlife_production
+from . import hr_employee
+
+
+
 from . import ir_rule
 from . import product_order_cost_line
 from . import purchase_order
-from . import forlife_event
-from . import forlife_production
-from . import account_move_line
+
+
+
 from . import product
-from . import barcode
+
 from . import res_partner
-from . import forlife_bom
+
 from . import stock_warehouse
 from . import product_supplierinfo
-from . import hr_employee
+
 from . import stock_move
 from . import production_history
 from . import production_material_import
+from . import stock_picking
