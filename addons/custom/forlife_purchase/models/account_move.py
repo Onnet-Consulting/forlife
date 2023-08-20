87c72374dacbb9ae58c1e6cc3df23f3a4910b3cb
Nguyen Van Huy <huy.nguyen@arrowhitech.com>
2023-08-20 08:23:42 +0700

    Fix update quantity_invoiced

--- addons/custom/forlife_purchase/models/account_move.py
+++ addons/custom/forlife_purchase/models/account_move.py
@@ -35,6 +35,11 @@ class AccountMove(models.Model):
                 rec.receiving_warehouse_id.write({
                     'ware_check': False
                 })
+            for invoice_line_id in rec.invoice_line_ids.filtered(lambda x: x.stock_move_id):
+                invoice_line_id.stock_move_id.write({
+                    'qty_invoiced': 0,
+                    'qty_refunded': 0,
+                })
         return super(AccountMove, self).button_cancel()
 
     def unlink(self):
@@ -43,9 +48,9 @@ class AccountMove(models.Model):
                 rec.receiving_warehouse_id.write({
                     'ware_check': False
                 })
-            for invoice_line_id in rec.invoice_line_ids:
+            for invoice_line_id in rec.invoice_line_ids.filtered(lambda x: x.stock_move_id):
                 invoice_line_id.stock_move_id.write({
-                    'qty_invoiced': invoice_line_id.stock_move_id.qty_invoiced - invoice_line_id.quantity,
+                    'qty_invoiced': 0,
                     'qty_refunded': 0,
                 })
         return super(AccountMove, self).unlink()
