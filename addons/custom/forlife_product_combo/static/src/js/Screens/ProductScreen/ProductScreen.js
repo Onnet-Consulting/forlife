17be3ea045cf7b53d21ec1d86685312ac7537abd
ducminh <ducminh>
2023-08-22 15:27:43 +0700

    fix

--- addons/custom/forlife_product_combo/static/src/js/Screens/ProductScreen/ProductScreen.js
+++ addons/custom/forlife_product_combo/static/src/js/Screens/ProductScreen/ProductScreen.js
@@ -94,38 +94,15 @@ odoo.define('forlife_product_combo.ProductScreen', function (require) {
                     if(product_valid_combo_in_pos.length != rsltObject[list_key[i]].length){
                         let intersection = self.getSameValueofArr(product_valid_combo_in_pos, rsltObject[list_key[i]])
                         order_lines.forEach(function(line){
-                            if(intersection.includes(line.product.product_tmpl_id)){
+                            if(intersection.includes(line.product.sku_code)){
                                 list_name.push(line.product.display_name)
                             }
                         })
                     }else {
                         for(let i=0; i< list_key.length; i++){
-                            let list_check = []
-                            let arr_name = []
-                            let arr_qty = []
-                            list_product_tmpl.forEach(function(item){
-                                for(let j=0; j<merge_combo[list_key[i]].length; j++){
-                                    if(item.sku_code == merge_combo[list_key[i]][j].sku_code){
-                                       list_check.push(item.quantity/merge_combo[list_key[i]][j].quantity)
-                                       arr_name.push(merge_combo[list_key[i]][j].product_name.vi_VN)
-                                       arr_qty.push([merge_combo[list_key[i]][j].quantity, merge_combo[list_key[i]][j].product_name.vi_VN])
-                                    }
-                                }
-                            })
-                            let msg = '';
-                            arr_qty.forEach(function(item, index){
-                                if(index != arr_qty.length-1){
-                                   msg += `${item.toString().replace(',',' sản phẩm ').replace(',\t',' sản phẩm ')} và `
-                                }else{
-                                   msg += `${item.toString().replace(',',' sản phẩm ').replace(',\t',' sản phẩm ')}`
-                                }
-                            })
-                            let body;
-                            if(!order.is_refund_product){
-                                body = this.env._t(`Sản phẩm ${arr_name.toString()} có số lượng chưa đúng so với cấu hình bộ.\n Gợi ý: ${msg} là một bộ!`);
-                            }else{
-                                body = this.env._t(`Sản phẩm ${arr_name.toString()} phải trả đúng với cấu hình bộ.\n Gợi ý: ${msg} là một bộ!`);
-                            }
+                            let value_check = this._function_validate_quantity(list_product_tmpl, list_key, merge_combo, i)
+                            let list_check = value_check[0]
+                            let body = value_check[1]
                             if(new Set(list_check).size !== 1){
                                 this.showPopup('ErrorPopup', {
                                     title: this.env._t('Error: Quantity Invalid!'),
@@ -138,46 +115,31 @@ odoo.define('forlife_product_combo.ProductScreen', function (require) {
                 }
             }else if(order.is_change_product){
                 for(let i=0; i< list_key.length; i++){
-                    let product_valid_combo_in_pos = []
-                    for(let j =0;j <list_product_tmpl.length; j++){
-                        if(rsltObject[list_key[i]].includes(list_product_tmpl[j].product_tmpl_id) && list_product_tmpl[j].refunded_orderline_id){
-                            product_valid_combo_in_pos.push(list_product_tmpl[j].product_tmpl_id)
-                        }
+                    let value_check = this._function_validate_quantity(list_product_tmpl, list_key, merge_combo, i)
+                    let list_check = value_check[0]
+                    let body = value_check[1]
+                    if(new Set(list_check).size !== 1){
+                        this.showPopup('ErrorPopup', {
+                            title: this.env._t('Error: Quantity Invalid!'),
+                            body: body,
+                        });
+                        return;
                     }
-//                    for(let i =0; i<list_product_tmpl)
-//                    var check_product_add_new = []
-//                    order_lines.forEach(function(item){
-//                        if(item.is_new_line){
-//                            check_product_add_new.push()
-//                        }
-//                    })
-//                    order_lines.forEach(function(line){
-//                        for(let k =0; k< list_product_tmpl.length; k++){
-//                            if(!line.refunded_orderline_id && !list_product_tmpl[k].refunded_orderline_id){
-//                                if(line.product.sku_code != list_product_tmpl[k].sku_code || (line.product.product_tmpl_id == list_product_tmpl[k].product_tmpl_id && line.product.id ==list_product_tmpl[k].product_id)){
-//                                    list_product_change_invalid.push(line.product.display_name)
-//                                }
-//                            }
-//                        }
-//                    })
-
-
-
                 }
             }
-//            if (list_name.length >0){
-//                let info_order;
-//                if(!order.is_refund_product){
-//                    info_order = 'Mua'
-//                }else{
-//                    info_order = 'Trả'
-//                }
-//                this.showPopup('ErrorPopup', {
-//                    title: this.env._t('Error: Product Invalid!'),
-//                    body: this.env._t(`Sản phẩm ${list_name.toString()} thuộc bộ nên cần hoàn thành bộ khi ${info_order}!`),
-//                });
-//                return;
-//            }
+            if (list_name.length >0){
+                let info_order;
+                if(!order.is_refund_product){
+                    info_order = 'Mua'
+                }else{
+                    info_order = 'Trả'
+                }
+                this.showPopup('ErrorPopup', {
+                    title: this.env._t('Error: Product Invalid!'),
+                    body: this.env._t(`Sản phẩm ${list_name.toString()} thuộc bộ nên cần hoàn thành bộ khi ${info_order}!`),
+                });
+                return;
+            }
 //            if(list_product_change_invalid.length >0){
 //                this.showPopup('ErrorPopup', {
 //                    title: this.env._t('Error: Product Invalid!'),
@@ -194,6 +156,40 @@ odoo.define('forlife_product_combo.ProductScreen', function (require) {
 //            }
             return await super._onClickPay(...arguments);
         }
+        _function_validate_quantity (list_product_tmpl, list_key, merge_combo, i) {
+            var order = this.env.pos.selectedOrder;
+            let list_check = []
+            let arr_name = []
+            let arr_qty = []
+            list_product_tmpl.forEach(function(item){
+                for(let j=0; j<merge_combo[list_key[i]].length; j++){
+                    if(item.sku_code == merge_combo[list_key[i]][j].sku_code){
+                       list_check.push(item.quantity/merge_combo[list_key[i]][j].quantity)
+                       arr_name.push(merge_combo[list_key[i]][j].product_name.vi_VN)
+                       arr_qty.push([merge_combo[list_key[i]][j].quantity, merge_combo[list_key[i]][j].product_name.vi_VN])
+                    }
+                }
+            })
+            let msg = '';
+            arr_qty.forEach(function(item, index){
+                if(index != arr_qty.length-1){
+                   msg += `${item.toString().replace(',',' sản phẩm ').replace(',\t',' sản phẩm ')} và `
+                }else{
+                   msg += `${item.toString().replace(',',' sản phẩm ').replace(',\t',' sản phẩm ')}`
+                }
+            })
+            let body;
+            if(!order.is_refund_product && !order.is_change_product){
+                body = this.env._t(`Sản phẩm ${arr_name.toString()} có số lượng chưa đúng so với cấu hình bộ.\n Gợi ý: ${msg} là một bộ!`);
+            }else{
+                body = this.env._t(`Sản phẩm ${arr_name.toString()} phải trả đúng với cấu hình bộ.\n Gợi ý: ${msg} là một bộ!`);
+            }
+            return [list_check, body]
+        }
+
+//        _function_validate_product () {
+//
+//        }
     };
 
     Registries.Component.extend(ProductScreen, ProductComboScreen);
