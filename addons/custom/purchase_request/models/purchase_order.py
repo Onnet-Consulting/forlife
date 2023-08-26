38effe3ae5509bdfc2d7ec6127e150ca2f5d7543
Nguyen Van Huy <huy.nguyen@arrowhitech.com>
2023-08-26 15:46:54 +0700

    Fix update account

--- addons/custom/purchase_request/models/purchase_order.py
+++ addons/custom/purchase_request/models/purchase_order.py
@@ -163,7 +163,7 @@ class PurchaseOrderLine(models.Model):
         return res
 
     def action_npl(self):
-        self.ensure_one()
+        # self.ensure_one()
         if not self.purchase_order_line_material_line_ids:
             product = self.product_id
             production_order = self.env['production.order'].search([('product_id', '=', product.id), ('type', '=', 'normal'), ('company_id', '=', self.env.company.id)], limit=1)
