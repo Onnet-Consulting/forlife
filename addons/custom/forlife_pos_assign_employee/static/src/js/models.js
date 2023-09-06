b6ec21530bef73d3cf6b1bc1f4681574d0f356bb
loilv <loilv.295@gmail.com>
2023-09-06 11:26:38 +0700

    Chít mã nhân viên trước

--- addons/custom/forlife_pos_assign_employee/static/src/js/models.js
+++ addons/custom/forlife_pos_assign_employee/static/src/js/models.js
@@ -1,15 +1,31 @@
 odoo.define('forlife_pos_assign_employee.models', function (require) {
     "use strict";
 
-    var {PosGlobalState, Orderline} = require('point_of_sale.models');
+    var {PosGlobalState, Orderline, Order} = require('point_of_sale.models');
     const Registries = require('point_of_sale.Registries');
+    const {useState} = owl;
 
 
     const PosCustomPosGlobalState = (PosGlobalState) => class extends PosGlobalState {
+
+        constructor(obj, options) {
+            super(...arguments);
+            this.employeeSelected = useState({
+                employeeID: {}
+            })
+        }
         async _processData(loadedData) {
             await super._processData(...arguments);
             this.assignable_employees = loadedData['assignable_employees'];
             this.assignable_employee_by_id = loadedData['assignable_employee_by_id'];
+        }
+
+        setDefaultEmployee(employee_id) {
+            this.employeeSelected.employeeID = employee_id
+        }
+
+        getDefaultEmployee() {
+            return this.employeeSelected.employeeID
         }
     }
     Registries.Model.extend(PosGlobalState, PosCustomPosGlobalState);
@@ -46,9 +62,22 @@ odoo.define('forlife_pos_assign_employee.models', function (require) {
             }
 
             get_employee() {
+                console.log(this.employee_id)
                 return this.employee_id;
             }
         }
+
+
+    const OrderAssignEmployee = (Order) => class extends Order {
+        add_orderline(line) {
+            if (line.pos.employeeSelected.employeeID) {
+                line.set_employee(line.pos.employeeSelected.employeeID)
+            }
+            super.add_orderline(...arguments);
+        }
+    }
+
+    Registries.Model.extend(Order, OrderAssignEmployee);
 
     Registries.Model.extend(Orderline, EmployeeOrderLine);
 });
