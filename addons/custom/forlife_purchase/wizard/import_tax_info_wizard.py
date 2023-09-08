5a09e7dae58f1b292872dab74f49ffa15d619631
Daihv <daihvls96@gmail.com>
2023-09-08 13:50:54 +0700

    [IMP] forlife_purchase: Thêm cột barcode trong file template import thông tin thuế

--- addons/custom/forlife_purchase/wizard/import_tax_info_wizard.py
+++ addons/custom/forlife_purchase/wizard/import_tax_info_wizard.py
@@ -4,7 +4,6 @@ from odoo import api, fields, models, _
 from odoo.exceptions import UserError, ValidationError
 import xlrd
 import base64
-import copy
 
 
 class ImportTaxInfoWizard(models.TransientModel):
@@ -25,7 +24,7 @@ class ImportTaxInfoWizard(models.TransientModel):
         sheet.freeze_panes(1, 0)
         sheet.set_row(0, 30)
         TITLES = [
-            'Database ID', 'Sản phẩm', 'Mô tả', 'Thành tiền VND', 'Số lượng', '% Thuế nhập khẩu', 'Thuế nhập khẩu',
+            'Database ID', 'Sản phẩm', 'Barcode', 'Mô tả', 'Thành tiền VND', 'Số lượng', '% Thuế nhập khẩu', 'Thuế nhập khẩu',
             '% Thuế tiêu thụ đặc biệt', 'Thuế tiêu thụ đặc biệt', '% Thuế GTGT', 'Thuế GTGT', 'Tổng tiền thuế'
         ]
         for idx, title in enumerate(TITLES):
@@ -35,20 +34,21 @@ class ImportTaxInfoWizard(models.TransientModel):
         for line in self.po_id.exchange_rate_line_ids:
             sheet.write(row, 0, line.id, formats.get('center_format'))
             sheet.write(row, 1, line.product_id.name or '', formats.get('normal_format'))
-            sheet.write(row, 2, line.description or '', formats.get('normal_format'))
-            sheet.write(row, 3, line.total_vnd_exchange or 0, formats.get('int_number_format'))
-            sheet.write(row, 4, line.product_qty or 0, formats.get('int_number_format'))
-            sheet.write(row, 5, line.import_tax or 0, formats.get('float_number_format'))
-            sheet.write(row, 6, line.tax_amount or 0, formats.get('int_number_format'))
-            sheet.write(row, 7, line.special_consumption_tax or 0, formats.get('float_number_format'))
-            sheet.write(row, 8, line.special_consumption_tax_amount or 0, formats.get('int_number_format'))
-            sheet.write(row, 9, line.vat_tax or 0, formats.get('float_number_format'))
-            sheet.write(row, 10, line.vat_tax_amount or 0, formats.get('int_number_format'))
-            sheet.write(row, 11, line.total_tax_amount or 0, formats.get('int_number_format'))
+            sheet.write(row, 2, line.product_id.barcode or '', formats.get('normal_format'))
+            sheet.write(row, 3, line.description or '', formats.get('normal_format'))
+            sheet.write(row, 4, line.total_vnd_exchange or 0, formats.get('int_number_format'))
+            sheet.write(row, 5, line.product_qty or 0, formats.get('int_number_format'))
+            sheet.write(row, 6, line.import_tax or 0, formats.get('float_number_format'))
+            sheet.write(row, 7, line.tax_amount or 0, formats.get('int_number_format'))
+            sheet.write(row, 8, line.special_consumption_tax or 0, formats.get('float_number_format'))
+            sheet.write(row, 9, line.special_consumption_tax_amount or 0, formats.get('int_number_format'))
+            sheet.write(row, 10, line.vat_tax or 0, formats.get('float_number_format'))
+            sheet.write(row, 11, line.vat_tax_amount or 0, formats.get('int_number_format'))
+            sheet.write(row, 12, line.total_tax_amount or 0, formats.get('int_number_format'))
             row += 1
 
     def get_filename(self):
-        return f"Đơn mua hàng nhập khẩu {self.po_id.name or ''}"
+        return f"Đơn mua hàng {self.po_id.name or ''}"
 
     @api.onchange('import_file')
     def onchange_import_file(self):
@@ -76,9 +76,9 @@ class ImportTaxInfoWizard(models.TransientModel):
                 error.append(f"Dòng {index}: ID '{data[0]}' không khớp với dữ liệu trong tab Thông tin thuế")
             elif not error:
                 old_tax_detail = old_tax_data.get(data[0])
-                import_tax = float(data[5])
-                special_consumption_tax = float(data[7])
-                vat_tax = float(data[9])
+                import_tax = float(data[6])
+                special_consumption_tax = float(data[8])
+                vat_tax = float(data[10])
                 if old_tax_detail[0] != import_tax or old_tax_detail[1] != special_consumption_tax or old_tax_detail[2] != vat_tax:
                     data_write.append((1, int(data[0]), {
                         'import_tax': import_tax,
