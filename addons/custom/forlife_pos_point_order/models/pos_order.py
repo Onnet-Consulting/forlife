3fec0636f1dc69e07df1abb9b690598003fea1be
Minh <minh.nguyen@onnet.vn>
2023-09-15 11:01:13 +0700

    MFL2201ER-1272-fix: fix bút toán trả điểm

--- addons/custom/forlife_pos_point_order/models/pos_order.py
+++ addons/custom/forlife_pos_point_order/models/pos_order.py
@@ -443,15 +443,17 @@ class PosOrder(models.Model):
     def action_point_refund(self, order_data):
         refund_point_list = []
         for line in order_data['data']['lines']:
-            if line[2]['refunded_orderline_id'] and line[2]['pos_order_line_discount_details']:
+            if line[2].get('refunded_orderline_id', False) and line[2].get('pos_order_line_discount_details', False):
                 for discount in line[2]['pos_order_line_discount_details']:
                     if discount['type'] == 'point':
                         refund_point_list.append((line[2]['qty'], discount))
         total_point = sum([abs(qty * p['money_reduced_unit']) for qty, p in refund_point_list])
-        point_journal_id = self.refunded_order_ids.program_store_point_id.account_journal_id
-        if len(point_journal_id) > 1:
-            point_journal_id = point_journal_id[0]
-        elif not point_journal_id:
+        refunded_order_id = self.refunded_order_ids
+        if len(self.refunded_order_ids) > 1:
+            refunded_order_id = self.refunded_order_ids[0]
+
+        point_journal_id = refunded_order_id.program_store_point_id.account_journal_id
+        if not point_journal_id:
             raise UserError(_('Cấu hình Sổ điểm cho chương trình điểm: %s',
                               self.refunded_order_ids.program_store_point_id.name or ''))
         if total_point > 0:
@@ -465,15 +467,15 @@ class PosOrder(models.Model):
                 'line_ids': [
                     # debit line
                     (0, 0, {
-                        'account_id': self.program_store_point_id.acc_accumulate_points_id.id,
-                        'partner_id': self.program_store_point_id.point_customer_id.id,
+                        'account_id': refunded_order_id.program_store_point_id.acc_accumulate_points_id.id,
+                        'partner_id': refunded_order_id.program_store_point_id.point_customer_id.id,
                         'debit': 0.0,
                         'credit': total_point,
                     }),
                     # credit line
                     (0, 0, {
-                        'account_id': self.program_store_point_id.point_customer_id.property_account_receivable_id.id,
-                        'partner_id': self.program_store_point_id.point_customer_id.id,
+                        'account_id': refunded_order_id.program_store_point_id.point_customer_id.property_account_receivable_id.id,
+                        'partner_id': refunded_order_id.program_store_point_id.point_customer_id.id,
                         'debit': total_point,
                         'credit': 0.0,
                     }),
