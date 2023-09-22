ad0b36f141eb5fc62a74dd16be743b05456df3f2
Minh <minh.nguyen@onnet.vn>
2023-09-22 15:52:44 +0700

    update translate vi

--- addons/custom/forlife_pos_app_member/models/store.py
+++ addons/custom/forlife_pos_app_member/models/store.py
@@ -5,4 +5,4 @@ from odoo import api, fields, models, _
 class Store(models.Model):
     _inherit = 'store'
 
-    contact_id = fields.Many2one('res.partner', string='Contact Store', required=True, domain=lambda self: [('group_id', 'in', self.env['res.partner.group'].search([('code', '=', '4000')]).ids)])
+    contact_id = fields.Many2one('res.partner', string='Contact Store', required=True, domain=lambda self: [('group_id', 'in', self.env['res.partner.group'].search([('code', '=', '500000')]).ids)])
