7cd86afbf9a9c7c90d44e85e8133dcb52ca0ac56
TienNQ <tienquang287@gmail.com>
2023-09-25 14:13:04 +0700

    [FIX] Fix bug forlife production

--- addons/custom/forlife_code_custom/models/account_move.py
+++ addons/custom/forlife_code_custom/models/account_move.py
@@ -1,4 +1,6 @@
 from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
 
 
 class AccountMove(models.Model):
@@ -12,30 +14,42 @@ class AccountMove(models.Model):
     @api.depends('posted_before', 'state', 'journal_id', 'date')
     def _compute_name(self):
         self = self.sorted(lambda m: (m.date, m.ref or '', m.id))
+        highest_name = self[0]._get_last_sequence(lock=False) if self else False
+
         for move in self:
-            
-            move_has_name = move.name and move.name != '/'
-            if move_has_name or move.state != 'posted':
-                if not move.posted_before and not move._sequence_matches_date():
-                    if move._get_last_sequence(lock=False):
-                        # The name does not match the date and the move is not the first in the period:
-                        # Reset to draft
-                        move.name = False
-                        continue
+            if not highest_name and move == self[0] and not move.posted_before and move.date and (not move.name or move.name == '/'):
+                # In the form view, we need to compute a default sequence so that the user can edit
+                # it. We only check the first move as an approximation (enough for new in form view)
+                declare_code_id = move._get_declare_code()
+                if not declare_code_id:
+                    move._set_next_sequence()
                 else:
-                    if move_has_name and move.posted_before or not move_has_name and move._get_last_sequence(lock=False):
-                        # The move either
-                        # - has a name and was posted before, or
-                        # - doesn't have a name, but is not the first in the period
-                        # so we don't recompute the name
-                        continue
-            if move.date and (not move_has_name or not move._sequence_matches_date()):
+                    move.name = declare_code_id.genarate_code('account_move','name')
+            elif move.quick_edit_mode and not move.posted_before:
+                # We always suggest the next sequence as the default name of the new move
+                declare_code_id = move._get_declare_code()
+                if not declare_code_id:
+                    move._set_next_sequence()
+                else:
+                    move.name = declare_code_id.genarate_code('account_move','name')
+            elif (move.name and move.name != '/') or move.state != 'posted':
+                try:
+                    move._constrains_date_sequence()
+                    # The name matches the date: we don't recompute
+                except ValidationError:
+                    # Has never been posted and the name doesn't match the date: recompute it
+                    declare_code_id = move._get_declare_code()
+                    if not declare_code_id:
+                        move._set_next_sequence()
+                    else:
+                        move.name = declare_code_id.genarate_code('account_move','name')
+            else:
+                # The name is not set yet and it is posted
                 declare_code_id = move._get_declare_code()
                 if not declare_code_id:
                     move._set_next_sequence()
                 else:
                     move.name = declare_code_id.genarate_code('account_move','name')
 
-        self.filtered(lambda m: not m.name and not move.quick_edit_mode).name = '/'
-        self._inverse_name()
+        self.filtered(lambda m: not m.name).name = '/'
 
