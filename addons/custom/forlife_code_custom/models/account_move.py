02bd85a7b363da324132ef1f587208e6eaea8f64
TienNQ <tienquang287@gmail.com>
2023-10-03 11:39:37 +0700

    [REF] Update forlife code document

--- addons/custom/forlife_code_custom/models/account_move.py
+++ addons/custom/forlife_code_custom/models/account_move.py
@@ -17,11 +17,14 @@ class AccountMove(models.Model):
         highest_name = self[0]._get_last_sequence(lock=False) if self else False
         sequence = 0
         for move in self:
+            check_origin_entry = False
+            if move.pos_order_id or move.pos_order_ids or (move.stock_valuation_layer_ids and move.stock_valuation_layer_ids[0].stock_move_id.picking_id and move.stock_valuation_layer_ids[0].stock_move_id.picking_id.picking_type_id.warehouse_id):
+                check_origin_entry = True
             if not highest_name and move == self[0] and not move.posted_before and move.date and (not move.name or move.name == '/'):
                 # In the form view, we need to compute a default sequence so that the user can edit
                 # it. We only check the first move as an approximation (enough for new in form view)
                 declare_code_id = move._get_declare_code()
-                if not declare_code_id:
+                if not declare_code_id or check_origin_entry:
                     move._set_next_sequence()
                 else:
                     move.name = declare_code_id.genarate_code(move.company_id.id,'account_move','name',sequence)
@@ -29,7 +32,7 @@ class AccountMove(models.Model):
             elif move.quick_edit_mode and not move.posted_before:
                 # We always suggest the next sequence as the default name of the new move
                 declare_code_id = move._get_declare_code()
-                if not declare_code_id:
+                if not declare_code_id or check_origin_entry:
                     move._set_next_sequence()
                 else:
                     move.name = declare_code_id.genarate_code(move.company_id.id,'account_move','name',sequence)
@@ -41,7 +44,7 @@ class AccountMove(models.Model):
                 except ValidationError:
                     # Has never been posted and the name doesn't match the date: recompute it
                     declare_code_id = move._get_declare_code()
-                    if not declare_code_id:
+                    if not declare_code_id or check_origin_entry:
                         move._set_next_sequence()
                     else:
                         move.name = declare_code_id.genarate_code(move.company_id.id,'account_move','name',sequence)
@@ -49,7 +52,7 @@ class AccountMove(models.Model):
             else:
                 # The name is not set yet and it is posted
                 declare_code_id = move._get_declare_code()
-                if not declare_code_id:
+                if not declare_code_id or check_origin_entry:
                     move._set_next_sequence()
                 else:
                     move.name = declare_code_id.genarate_code(move.company_id.id,'account_move','name',sequence)
