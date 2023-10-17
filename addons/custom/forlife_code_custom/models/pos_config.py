9f6f7b7cd10de634a3b6268cd6015b81680ba04f
taiht <taiht>
2023-10-17 16:34:59 +0700

    fix account move name

--- addons/custom/forlife_code_custom/models/pos_config.py
+++ addons/custom/forlife_code_custom/models/pos_config.py
@@ -5,14 +5,14 @@ from odoo import api, fields, models, _
 class PosConfig(models.Model):
     _inherit = 'pos.config'
 
-    pos_id = fields.Integer('pos_id', compute='_compute_pos_id', store=True)
+    pos_id = fields.Char('pos_id', compute='_compute_pos_id', store=True)
 
     @api.depends('store_id', 'store_id.pos_config_ids')
     def _compute_pos_id(self):
         for rec in self:
+            pos_id = 0
             if rec.store_id:
                 pos_ids = rec.store_id.pos_config_ids.ids
                 pos_ids.sort()
-                rec.pos_id = pos_ids.index(rec.id) if rec.id and rec.id in pos_ids else len(pos_ids)
-            else:
-                rec.pos_id = 0
+                pos_id = pos_ids.index(rec.id) if rec.id and rec.id in pos_ids else len(pos_ids)
+            rec.pos_id = '0'+str(pos_id) if pos_id < 10 else str(pos_id)
