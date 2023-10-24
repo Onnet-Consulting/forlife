0051d2f44255a45b0b31a12f30ba0f0a02e4ebee
Minh <minh.nguyen@onnet.vn>
2023-10-24 11:46:02 +0700

    pos-required-accounting-field

--- addons/custom/forlife_pos_accounting/models/promotion_program.py
+++ addons/custom/forlife_pos_accounting/models/promotion_program.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo.exceptions import UserError
+from odoo import fields, models, api, _
 
 
 class InheritPromotionProgram(models.Model):
@@ -9,3 +10,11 @@ class InheritPromotionProgram(models.Model):
         comodel_name='product.product', string='Discount Product', index=True,
         domain="[('is_promotion', '=', True)]"
     )
+
+    @api.constrains('journal_id', 'product_discount_id')
+    def _check_required_accounting_fields(self):
+        for record in self:
+            if not record.journal_id:
+                raise UserError(_('Thiếu cấu hình Sổ nhật ký !'))
+            if not record.product_discount_id:
+                raise UserError(_('Thiếu cấu hình sản phẩm khyuến mãi !'))
