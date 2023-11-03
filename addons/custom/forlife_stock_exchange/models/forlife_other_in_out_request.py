c320053b9424de1d16085dc4fe7c85c197cbb74d
Nguyen Van Huy <huy.nguyen@arrowhitech.com>
2023-11-03 11:17:41 +0700

    Remove approve other request

--- addons/custom/forlife_stock_exchange/models/forlife_other_in_out_request.py
+++ addons/custom/forlife_stock_exchange/models/forlife_other_in_out_request.py
@@ -4,20 +4,20 @@ from odoo import models
 class InheritForlifeOtherInOutRequest(models.Model):
     _inherit = 'forlife.other.in.out.request'
 
-    def action_approve(self):
-        result = super(InheritForlifeOtherInOutRequest, self).action_approve()
-        ForlifeReasonType = self.env['forlife.reason.type']
-        company = self.env.company
-        reason_type_incoming_exchange_id = ForlifeReasonType.search([('code', '=', 'N01'), ('company_id', '=', company.id)], limit=1)
-        # forlife_reason_type_incoming_exchange = self.env.ref('forlife_stock_exchange.forlife_reason_type_incoming_exchange')
-        pickings = self.env['stock.picking'].sudo().search(
-            [('other_import_export_request_id', 'in', [rec.id for rec in self if rec.type_other_id == reason_type_incoming_exchange_id])],
-        )
-        if pickings:
-            picking_type_id = self.env['stock.picking.type'].sudo().search([('exchange_code', '=', 'incoming')], limit=1)
-            if picking_type_id:
-                pickings.write({'picking_type_id': picking_type_id.id})
-        return result
+    # def action_approve(self):
+    #     result = super(InheritForlifeOtherInOutRequest, self).action_approve()
+    #     ForlifeReasonType = self.env['forlife.reason.type']
+    #     company = self.env.company
+    #     reason_type_incoming_exchange_id = ForlifeReasonType.search([('code', '=', 'N01'), ('company_id', '=', company.id)], limit=1)
+    #     # forlife_reason_type_incoming_exchange = self.env.ref('forlife_stock_exchange.forlife_reason_type_incoming_exchange')
+    #     pickings = self.env['stock.picking'].sudo().search(
+    #         [('other_import_export_request_id', 'in', [rec.id for rec in self if rec.type_other_id == reason_type_incoming_exchange_id])],
+    #     )
+    #     if pickings:
+    #         picking_type_id = self.env['stock.picking.type'].sudo().search([('exchange_code', '=', 'incoming')], limit=1)
+    #         if picking_type_id:
+    #             pickings.write({'picking_type_id': picking_type_id.id})
+    #     return result
 
     def action_other_import_export(self):
         ref = self.env.ref
